ee15ea2c35c8abafa3016f862d5896eb983e5e6d
michael35040 <michaelsjohnson@gmail.com>
2014-10-31 08:01:15 -0400

    Update information_symbol_form.php

--- templates/information_symbol_form.php
+++ templates/information_symbol_form.php
@@ -3,6 +3,9 @@
     <fieldset>
         <!-- removed autofocus             <input autofocus name="symbol" placeholder="Symbol" type="text" maxlength="31"/> -->
 
+ <div class="input-append">
+    <div class="input-prepend">
+
         <input list="symbol" name="symbol" maxlength="8" class="input-small" required><!--<input list="symbol" class="input-small" name="symbol" id="symbol" placeholder="Symbol" type="text" maxlength="5" required-->
         <datalist id="symbol"><!--select class="input-small" name="symbol" id="symbol" /-->
 
@@ -17,13 +20,11 @@
             }
             ?>
         </datalist>
-
-        <BR><BR>Exchange:<BR>
-
-
+    </div><!--input-prepend-->
+    <div class="input-prepend">
         <button type="submit" class="btn btn-danger" >Submit</button><!--national exchange-->
-
-
+    </div><!--input-prepend-->
+</div><!--input-append-->
 
     </fieldset>
 </form>
