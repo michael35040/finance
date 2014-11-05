884651329f08145364b5acca18e2b6bff108a9cd
michael35040 <michaelsjohnson@gmail.com>
2014-11-04 22:30:29 -0500

    1

--- includes/bank.sql
+++ includes/bank.sql
@@ -52,6 +52,30 @@ TRUNCATE TABLE `accounts`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `assets`
+--
+
+DROP TABLE IF EXISTS `assets`;
+CREATE TABLE IF NOT EXISTS `assets` (
+  `uid` int(11) NOT NULL,
+  `symbol` varchar(10) NOT NULL COMMENT 'ticker',
+  `name` varchar(63) NOT NULL,
+  `date` date NOT NULL COMMENT 'listed on',
+  `owner` varchar(63) NOT NULL,
+  `fee` decimal(65,30) NOT NULL COMMENT 'fee surcharge %',
+  `issued` int(11) NOT NULL COMMENT 'shares issued ie 20k',
+  `dividend` decimal(65,30) NOT NULL,
+  `url` varchar(63) NOT NULL COMMENT 'webpage',
+  `type` varchar(63) NOT NULL COMMENT 'shares or commodity',
+  `rating` int(11) NOT NULL COMMENT '4 stars or white',
+  `description` varchar(999) NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `symbol` (`symbol`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `error`
 --
 
@@ -275,6 +299,9 @@ TRUNCATE TABLE `users`;
 --
 -- Dumping data for table `users`
 --
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `phone`, `registered`, `last_login`, `ip`, `fails`, `active`) VALUES
+(1, 'a', 'a@a', '$2a$11$ZIiqSdMJtMeW4xWTTQl7zueNNfjw1w.qpoJ03E5AGRfSttU7GJQn2', 1, 1414334245, 2147483647, '::1', 0, 1),
+(2, 'b', 'b@b', '$2a$11$lorocz4ub9L3hy27HldufufXp3P6Z0lGb7YEO4ya3jx77SZTCtz7C', 1, 1414334258, 2147483647, '::1', 0, 1);
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
