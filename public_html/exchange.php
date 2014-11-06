d2a28d97f4e6ff393681d74f6367687d5a24d781
michael35040 <michaelsjohnson@gmail.com>
2014-11-06 13:03:53 -0500

    Update exchange.php

--- public_html/exchange.php
+++ public_html/exchange.php
@@ -1,35 +1,23 @@
 <?php
-
 require("../includes/config.php");  // configuration
-//require("../includes/constants.php"); //global finance constants
 $id = $_SESSION["id"]; //get id from session
-
-//var_dump(get_defined_vars()); //dump all variables if i hit error
-
 if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
 {
-    /////////////////////
-    //VARS FOR EITHER NATIONAL OR LOCAL
-    ////////////////////
-    //CHANGES POST VAR TO LOCAL
-
     @$symbol = $_POST["symbol"];	//assign post variables to local variables, not really needed but makes coding easier
     @$type = $_POST["type"]; //limit or market
     @$side = $_POST["side"]; //buy/bid or sell/ask 
     @$quantity = (int)$_POST["quantity"]; //not set on market orders
     @$price = (float)$_POST["price"]; //not set on market orders
 
-
-list($transaction, $symbol, $tradeTotal, $quantity, $commissionTotal) = placeOrder($symbol, $type, $side, $quantity, $price, $id);
-@$tradeTotal = (float)$tradeTotal; //convert string to float
-@$commissionTotal = (float)$commissionTotal; //convert string to float
-@$quantity = (int)$quantity; //convert string to float
-
-
+     if (empty($symbol) || empty($price) || empty($quantity) || empty($type) || empty($side)) { apologize("Incomplete form!"); } //check to see if empty
+    
+    list($transaction, $symbol, $tradeTotal, $quantity, $commissionTotal) = placeOrder($symbol, $type, $side, $quantity, $price, $id);
+    @$tradeTotal = (float)$tradeTotal; //convert string to float
+    @$commissionTotal = (float)$commissionTotal; //convert string to float
+    @$quantity = (int)$quantity; //convert string to float
+    
     render("success_form.php", ["title" => "Success", "transaction" => $transaction, "symbol" => $symbol, "value" => $tradeTotal, "quantity" => $quantity, "commissiontotal" => $commissionTotal]); // render success form
-
-
-} //if post
+    } //if post
 else
 {
     $stocks =	query("SELECT * FROM portfolio WHERE id = ? ORDER BY symbol ASC", $id);	  // query user's portfolio
