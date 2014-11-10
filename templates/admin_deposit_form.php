75f40e311a90646faf6b5332bdb0d95f19327d79
michael35040 <michaelsjohnson@gmail.com>
2014-11-10 13:13:56 -0500

    Update admin_deposit_form.php

--- templates/admin_deposit_form.php
+++ templates/admin_deposit_form.php
@@ -5,27 +5,10 @@
 
         <h3>Deposit:</h3>
 
-        <div class="input-append">
+<br /><input class="input-small" name="userid" placeholder="User ID" type="number" min="0" max="any" required />
+<br /><input class="input-medium"  type="number" name="quantity" placeholder="<?php echo($unitsymbol);?> Amount/Quantity" step="0.0000000000001" min="0" max="any" required />
+<br /><button type="submit" class="btn btn-success"><b>DEPOSIT</b></button>
 
-            <div class="input-prepend">
-                <span class="add-on"><i class="icon-download-alt"></i></span>
-
-            </div><!--input-prepend-->
-
-            <div class="input-prepend">
-                <input class="input-small" name="userid" placeholder="User ID" type="number" min="0" max="any" required />
-            </div><!--input-prepend-->
-
-            <div class="input-prepend">
-                <input class="input-medium"  type="number" name="quantity" placeholder="<?php echo($unitsymbol);?> Amount/Quantity" step="0.0000000000001" min="0" max="any" required />
-            </div><!--input-prepend-->
-            <br />
-            <button type="submit" class="btn btn-success"><b>DEPOSIT</b></button>
-
-        </div><!--input-append-->
-
-
-
-
-        <?php //var_dump(get_defined_vars()); //dump all variables anywhere (displays in header) ?>
+</fieldset>
+</form>
         <br />
