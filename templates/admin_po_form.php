ecdd56968b7d2512c9b05be3a8ce07bc0933c146
michael35040 <michaelsjohnson@gmail.com>
2014-11-13 13:11:04 -0500

    Update admin_po_form.php

--- templates/admin_po_form.php
+++ templates/admin_po_form.php
@@ -45,8 +45,10 @@
         <tr>
             <td style="width:20%">Symbol</td>
             <td style="width:80%">
-            <input type="text" name="symbol" maxlength="8" placeholder="ex: ABCD" required><br>
-            <input type="text" name="symbolConfirmation" maxlength="8" placeholder="Confirm Symbol" required> Must be same for initial. For follow-on/update, you can use this to change symbol.
+            <input type="text" name="symbol" maxlength="8" placeholder="ex: ABCD" required><br><br>
+            <input type="text" name="symbolConfirmation" maxlength="8" placeholder="Confirm Symbol" required><br> 
+            Must be same for initial. For follow-on/update, <br>
+            you can use this to change symbol.
             </td>
         </tr>
         <tr>
