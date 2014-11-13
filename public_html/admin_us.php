49bf76cc874bced935645993359a9b3b32b51218
michael35040 <michaelsjohnson@gmail.com>
2014-11-13 14:17:57 -0500

    Update admin_us.php

--- public_html/admin_us.php
+++ public_html/admin_us.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
     @$symbol = $_POST["symbol"];
     @$newSymbol = $_POST["newSymbol"];
     @$name = $_POST["name"];
-    //@$userid = $_POST["userid"]; //owner or chief executive
+    @$userid = $_POST["userid"]; //owner or chief executive
     @$owner = $_POST["owner"]; //email of owner or chief executive
     //@$fee = $_POST["fee"]; //fee
     //@$issued = $_POST["issued"]; //current amount of shares made public, issued for IPO
@@ -18,8 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
     @$type = $_POST["type"]; //share or commodity
     @$rating = $_POST["rating"]; //1 - 10
     @$description = $_POST["description"];
-    updateSymbol($symbol, $newSymbol, $name, $type, $owner, $url, $rating, $description)
-try {$message = updateSymbol($symbol, $newSymbol, $name, $type, $owner, $url, $rating, $description);}
+ 
+try {$message = updateSymbol($symbol, $newSymbol, $userid, $name, $type, $owner, $url, $rating, $description);}
 catch(Exception $e) {echo 'Message: ' .$e->getMessage();}
 
 //redirect("assets.php", ["title" => "Success"]); // render success form
