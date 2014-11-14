86e48b818eda016c0fa3823a6de63813b892374b
michael35040 <michaelsjohnson@gmail.com>
2014-11-14 15:16:20 -0500

    Update admin_activate.php

--- public_html/admin_activate.php
+++ public_html/admin_activate.php
@@ -31,84 +31,10 @@ $limit = "LIMIT 0, 10";
 $inactiveUsers = query("SELECT id, username, registered FROM users WHERE active=0 ORDER BY id ASC");
 $activeUsers = query("SELECT id, username, registered FROM users WHERE active=1 ORDER BY id ASC");
 
-//render("admin_activate.php", ["title" => $title, "inactiveUsers" => $inactiveUsers, "activeUsers" => $activeUsers]);
+render("admin_activate_form.php", ["title" => $title, "inactiveUsers" => $inactiveUsers, "activeUsers" => $activeUsers]);
 } //else !post
 ?>
 
 
 
 
-
-
-<table class="table table-condensed table-bordered">
-<tr class="success">
-  <td colspan="9" style="font-size:20px; text-align: center;">INACTIVE USERS</td>
-</tr>
-<!--blank row breaker-->
-<tr class="active">
-  <th>Action</th>
-  <th>User</th>
-</tr>
-<?php
-//list users
-$i=0;
-foreach ($inactiveUsers as $user)
-{ $i++;
-   echo('
-   <tr>
-     <td>
-      <form><span class="input-group-btn">
-      <button type="submit" class="btn btn-success btn-xs" formmethod="post" formaction="admin_activate.php" name="activate" value="' . $user["id"] . '">
-      <span class="glyphicon glyphicon-plus-sign"></span> Activate</button></span></form>
-    </td>
-    <td>' . $user["id"] . '</td></tr>');
-}
-if($i>0)
-{
-?>
-<tr><td>
-      <form><span class="input-group-btn">
-      <button type="submit" class="btn btn-success btn-xs" formmethod="post" formaction="admin_activate.php" name="activate" value="ALL">
-      <span class="glyphicon glyphicon-plus-sign"></span> Activate</button></span></form>
-</td>
-<td>ALL</td></tr>
-</table>
-<?php } ?>
-
-
-
-
-<table class="table table-condensed table-bordered">
-<tr class="success">
-  <td colspan="9" style="font-size:20px; text-align: center;">ACTIVE USERS</td>
-</tr>
-<!--blank row breaker-->
-<tr class="active">
-  <th>Action</th>
-  <th>User</th>
-</tr>
-<?php
-//list users
-$i=0;
-foreach ($activeUsers as $user)
-{ $i++;
-   echo('
-   <tr>
-     <td>
-      <form><span class="input-group-btn">
-      <button type="submit" class="btn btn-success btn-xs" formmethod="post" formaction="admin_activate.php" name="deactivate" value="' . $user["id"] . '">
-      <span class="glyphicon glyphicon-plus-sign"></span> Activate</button></span></form>
-    </td>
-    <td>' . $user["id"] . '</td></tr>');
-}
-if($i>0)
-{
-?>
-<tr><td>
-      <form><span class="input-group-btn">
-      <button type="submit" class="btn btn-success btn-xs" formmethod="post" formaction="admin_activate.php" name="deactivate" value="ALL">
-      <span class="glyphicon glyphicon-plus-sign"></span> Activate</button></span></form>
-</td>
-<td>ALL</td></tr>
-</table>
-<?php } ?>
