d995fbd869cb6d5c4710d0234c597b092334aeca
michael35040 <michaelsjohnson@gmail.com>
2014-11-14 09:38:24 -0500

    Update information_form.php

--- templates/information_form.php
+++ templates/information_form.php
@@ -251,10 +251,10 @@ if(isset($trades[0]["price"])) {$tradesPrice=$trades[0]["price"];}else{$tradesPr
         '<br>Type: ' . htmlspecialchars(ucfirst($asset["type"])) .
         '</td>');
     echo('</tr>');
-    echo('<tr class="success"><td colspan="4">Portfolio: ' . number_format($asset["quantity"]), 0, '.', ',') . '%</td></tr>');
-    echo('<tr class="success"><td colspan="4">Orderbook: ' . number_format($asset["locked"]), 0, '.', ',') . '%</td></tr>');
-    echo('<tr class="success"><td colspan="4">Total: ' . number_format($asset["locked"]+$asset["quantity"]), 0, '.', ',') . '%</td></tr>');
-    echo('<tr class="success"><td colspan="4">Control: ' . number_format(($asset["control"]), 0, '.', '') . '%</td></tr>');
+    echo('<tr class="success"><td colspan="4">Portfolio: ' . number_format($asset["quantity"]), 0, ".", ",") . '%</td></tr>');
+    echo('<tr class="success"><td colspan="4">Orderbook: ' . number_format($asset["locked"]), 0, ".", ",") . '%</td></tr>');
+    echo('<tr class="success"><td colspan="4">Total: ' . number_format($asset["locked"]+$asset["quantity"]), 0, ".", ",") . '%</td></tr>');
+    echo('<tr class="success"><td colspan="4">Control: ' . number_format(($asset["control"]), 0, ".","") . '%</td></tr>');
     echo('<tr><td colspan="4">Description: ' . htmlspecialchars(ucfirst($asset["description"])) . '</td></tr>');
     echo('</div>'); 
     ?>
