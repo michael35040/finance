ebfdae828bc9ff4616f7923e4b4c7795e0277f6f
michael35040 <michaelsjohnson@gmail.com>
2014-11-14 15:37:04 -0500

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,13 @@ require("../includes/config.php");
 $id = $_SESSION["id"]; //get id from session
 
 
+$purchaseprice = query("SELECT SUM(price) AS purchaseprice FROM portfolio WHERE id = ?", $_SESSION["id"]); //calculate purchase price
+
+$bidLocked =	query("SELECT SUM(total) AS total FROM orderbook WHERE (id=? AND side='b')", $id); // query user's portfolio
+$bidLocked = $bidLocked[0]["total"]; //shares trading
+
+
 // render portfolio (pass in new portfolio table and cash)
 render("index_form.php", ["title" => "Accounts"]);
-
+render("portfolio_form.php", ["title" => "Accounts", "purchaseprice" => $purchaseprice, "bidLocked" => $bidLocked]);
 ?>
