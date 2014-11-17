809719de8390d84ce14a2e497be9b46de6cd8f89
michael35040 <michaelsjohnson@gmail.com>
2014-11-17 13:49:35 -0500

    Update metals.php

--- public_html/metals.php
+++ public_html/metals.php
@@ -1,7 +1,7 @@
 <?php
-$xmldata = "https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/au/";
+$url = "https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/au/";
 //https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/ag/
 
-$xml=simplexml_load_string($xmldata) or die("Error: Cannot create object");
+$xml=simplexml_load_string(file_get_contents($url)) or die("Error: Cannot create object");
 print_r($xml);
 ?>
