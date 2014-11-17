27e28436e7a2f32fc597073329ba7c7ee5f3a528
michael35040 <michaelsjohnson@gmail.com>
2014-11-17 10:11:47 -0500

    Update change.php

--- public_html/change.php
+++ public_html/change.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				if (empty($_POST["phone"]) || empty($_POST["confirmation"])){apologize("You must provide a phone.");}
 				if ($_POST["phone"] != $_POST["confirmation"]){ apologize("Missmatch. The new phone does not match its confirmation."); }       
 				$phone = $_POST["phone"];
-				$phone=sanatize($phone);
+				$phone=sanatize("phone", $phone); //functions.php
 				if (!ctype_digit($phone)) { apologize("Phone must be numeric!");} //if quantity is numeric	
 				if(query("UPDATE users SET phone = ( ? ) WHERE id = ( ? )", $phone, $_SESSION["id"]) === false){ apologize("Sorry, username already taken."); }
 			}//phone
