445c0b27e7c03c6e5f91e11617ab6f8034b3907c
michael35040 <michaelsjohnson@gmail.com>
2014-11-17 08:08:40 -0500

    Update orders.php

--- public_html/orders.php
+++ public_html/orders.php
@@ -3,52 +3,36 @@ require("../includes/config.php");
 
 $id =  $_SESSION["id"];
 $title = "Open Orders";
-$limit = "LIMIT 0, 50";
+$limit = "LIMIT 0, 10";
+$tabletitle = "Last 10";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
 {
     if (isset($_POST["cancel"]))
-        //if(empty($_POST["cancel"])){apologize("No order selected!");}
-        //else
     {
         $uid = $_POST["cancel"];
         if ($uid == 'ALL') { //CANCEL ALL USERS ORDERS
-            if (query("UPDATE orderbook SET type = 'cancel' WHERE id = ?", $id) === false) {
-                apologize("Unable to cancel all orders!");
-            }
-        } else { //CANCEL ONLY 1 ORDER
-            if (query("UPDATE orderbook SET type = 'cancel' WHERE uid = ?", $uid) === false) {
-                apologize("Unable to cancel order!");
-            }
-        }
-        redirect('orders.php');
+            if (query("UPDATE orderbook SET type = 'cancel' WHERE id = ?", $id) === false) {apologize("Unable to cancel all orders!");}} 
+        else { //CANCEL ONLY 1 ORDER
+            if (!is_numeric($uid)){apologize("Invalid order #");}
+            if (query("UPDATE orderbook SET type = 'cancel' WHERE uid = ?", $uid) === false) {apologize("Unable to cancel order!");}}
     }
-    if (isset($_POST["history"])) {
+    if (isset($_POST["history"])) 
+    {
         $history = $_POST["history"];
-        if ($history == "all") {
-            $limit = "";
-            $tabletitle = "All";
-            $orders = query("SELECT * FROM orderbook WHERE (id = ?) ORDER BY uid DESC", $id);
-            $history = query("SELECT * FROM history WHERE id = ? ORDER BY uid DESC $limit", $id);
-            render("orders_form.php", ["title" => $title, "tabletitle" => $tabletitle, "orders" => $orders, "history" => $history]);
-        }
-        if ($history == "limit") {
-            $limit = "LIMIT 0, 10";
-            $tabletitle = "Last 10";
-            $orders = query("SELECT * FROM orderbook WHERE (id = ?) ORDER BY uid DESC", $id);
-            $history = query("SELECT * FROM history WHERE id = ? ORDER BY uid DESC $limit", $id);
-            render("orders_form.php", ["title" => $title, "tabletitle" => $tabletitle, "orders" => $orders, "history" => $history]);
-        }
+        if ($history == "all") {$limit = ""; $tabletitle = "All";}
     }
 }
+/*
 else
 {
-    $limit = "LIMIT 0, 10";
-    $tabletitle = "Last 10";
-    $orders = query("SELECT * FROM orderbook WHERE (id = ?) ORDER BY uid DESC", $id);
-    $history = query("SELECT * FROM history WHERE id = ? ORDER BY uid DESC $limit", $id);
-    render("orders_form.php", ["title" => $title, "tabletitle" => $tabletitle, "orders" => $orders, "history" => $history]);
 
-} //else !post
+} //else !post , 
+*/
+$orders = query("SELECT uid, date, symbol, side, type, quantity, price, total FROM orderbook WHERE (id = ?) ORDER BY uid DESC", $id);
+$ordertotal = query("SELECT SUM(total) AS sumtotal FROM orderbook WHERE (id = ?)", $id);
+$history = query("SELECT ouid, date, symbol, transaction, total FROM history WHERE id = ? ORDER BY uid DESC $limit", $id);
+render("orders_form.php", ["title" => $title, "tabletitle" => $tabletitle, "orders" => $orders,  "ordertotal" => $ordertotal, "history" => $history]);
+
 ?>
 
