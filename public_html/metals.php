792709eb5f2f1ce3cd7c88d62c52ba019175e614
michael35040 <michaelsjohnson@gmail.com>
2014-11-17 16:05:40 -0500

    Update metals.php

--- public_html/metals.php
+++ public_html/metals.php
@@ -2,9 +2,13 @@
 include '../includes/config.php'; 
 //https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/ag/
 
-$data = file_get_contents('https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/ag/');
-$xml = simplexml_load_file('https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/ag/');
+$data1 = file_get_contents('https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/ag/');
+$xml2 = simplexml_load_file('https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/ag/');
+
+
+$url = 'https://svc.cloudhost365.com/SharedServices/api/market/spotprices/USD/ag/';
+$xml = file_get_contents($url);
+$data = new SimpleXMLElement($xml);
+
 echo(var_dump(get_defined_vars()));
-
-
 ?>
