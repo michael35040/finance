4b9288f86d8488f8455a1963b19c95274226eaa2
michael35040 <michaelsjohnson@gmail.com>
2014-11-17 10:25:57 -0500

    Update change.php

--- public_html/change.php
+++ public_html/change.php
@@ -19,9 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				if (empty($_POST["email"]) || empty($_POST["confirmation"])){apologize("You must provide a email.");}
 				if ($_POST["email"] != $_POST["confirmation"]){apologize("Missmatch. The new email does not match its confirmation.");}
 				$email = $_POST["email"];
-				$email = filter_input(INPUT_POST, $email, FILTER_SANITIZE_EMAIL);
-				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { apologize("The email address you entered is not valid."); }   // Not a valid email
-				if (query("UPDATE users SET email = ( ? ) WHERE id = ( ? )", ($_POST["email"]), $_SESSION["id"]) === false) {apologize("Sorry, some internal error ocured.");}
+				$email = sanatize("email", $email);
+				if (query("UPDATE users SET email = ( ? ) WHERE id = ( ? )", ($email), $_SESSION["id"]) === false) {apologize("Sorry, some internal error ocured.");}
 			}//email
 			if ($change == 'phone')
 			{	
@@ -32,6 +31,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				if (!ctype_digit($phone)) { apologize("Phone must be numeric!");} //if quantity is numeric	
 				if(query("UPDATE users SET phone = ( ? ) WHERE id = ( ? )", $phone, $_SESSION["id"]) === false){ apologize("Sorry, username already taken."); }
 			}//phone
+			if ($change == 'username')
+			{	
+				if (empty($_POST["username"])){apologize("You must provide a username.");}
+				if (empty($_POST["confirmation"])){apologize("You must re-type your username.");}
+				if ($_POST["username"] != $_POST["confirmation"]){apologize("Missmatch. The new username does not match its confirmation.");}       
+				$username = $_POST["username"];
+				$username = sanatize("username", $username);
+				if(query("UPDATE users SET username = ( ? ) WHERE id = ( ? )", ($username), $_SESSION["id"]) === false){apologize("Sorry, username already taken.");}
+			}//username
 			if ($change == 'password')
 			{	
 				if (empty($_POST["password"]))  // validate submission{apologize("You must provide your old password.");}
@@ -39,14 +47,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				if ($_POST["newpassword"] != $_POST["confirmation"]){apologize("Missmatch. The new password does not match its Confirmation.");}
 				if(query("UPDATE users SET password = ? WHERE id = ? ", generate_hash($_POST["newpassword"]), $_SESSION["id"]) === false) {apologize("Sorry, some internal error ocured.");}
 			}//password
-			if ($change == 'username')
-			{	
-				if (empty($_POST["username"])){apologize("You must provide a username.");}
-				if (empty($_POST["confirmation"])){apologize("You must re-type your username.");}
-				if (!ctype_alnum($_POST["username"])) {apologize("Usernames must be alphanumeric (A-Z and/or 0-9)!");}
-				if ($_POST["username"] != $_POST["confirmation"]){apologize("Missmatch. The new username does not match its confirmation.");}       
-				if(query("UPDATE users SET username = ( ? ) WHERE id = ( ? )", ($_POST["username"]), $_SESSION["id"]) === false){apologize("Sorry, username already taken.");}
-			}//username
 			redirect("change.php");
 		} //password check is correct
 		else // (crypt($_POST["password"], $row["password"]) != $row["password"])
