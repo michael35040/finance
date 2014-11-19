322e1ac6ef5b473c943adf388558db29c4e800a5
michael35040 <michaelsjohnson@gmail.com>
2014-11-19 09:24:30 -0500

    Update instatrade.php

--- public_html/instatrade.php
+++ public_html/instatrade.php
@@ -5,10 +5,11 @@ require("../includes/config.php");
 // if form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-
-
+//placeOrder(Market,$symbol,$quantity)
 }
 else{
+    //$goldAsk;$goldBid;$silverAsk;$silverBid;$premium;$discount
+    
     $userinfo = query("SELECT username, email, phone FROM users WHERE id = ( ? )", $_SESSION["id"]);
     render("instatrade_form.php", ["title" => "Instant Trade", "userinfo" => $userinfo]);}// else render form
 
