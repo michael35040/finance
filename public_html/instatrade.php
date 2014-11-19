5e70576074c33c9e1c0971a1529a8274f2fb8f11
michael35040 <michaelsjohnson@gmail.com>
2014-11-19 14:53:32 -0500

    Update instatrade.php

--- public_html/instatrade.php
+++ public_html/instatrade.php
@@ -13,24 +13,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
     @$metalTransaction = $_POST["metalTransaction"];// buyGold, buySilver, sellGold, sellSilver
     
-    apologize(var_dump(get_defined_vars()));
+    //apologize(var_dump(get_defined_vars()));
     
         if($metalTransaction=='buyGold'){$symbol='GOLD';$side='bid';}
         elseif($metalTransaction=='buySilver'){$symbol='SILVER';$side='bid';}
         elseif($metalTransaction=='sellGold'){$symbol='GOLD';$side='ask';}
         elseif($metalTransaction=='sellSilver'){$symbol='SILVER';$side='ask';}
-        else{apologize(var_dump(get_defined_vars()));}// //dump all variables if i hit error
+        else{apologize('Unknown action!');}// //dump all variables if i hit error
     //FORMATS AND SCRUBS VARIABLES
     $price = sanatize("price", $price);
     $quantity = sanatize("quantity", $quantity);
     $symbol = strtoupper($symbol); //cast to UpperCase
+    
     if($symbol!='GOLD' || $symbol!='SILVER'){apologize('Invalid metal type! Gold or Silver only.');}
     $type = 'market';
     $side = sanatize("alphabet", $side);
-    try {
-    list($transaction, $symbol, $tradeTotal, $quantity, $commissionTotal) = placeOrder($symbol, $type, $side, $quantity, $price, $id);
-    }
+    
+    try {list($transaction, $symbol, $tradeTotal, $quantity, $commissionTotal) = placeOrder($symbol, $type, $side, $quantity, $price, $id);}
     catch(Exception $e) {apologize($e->getMessage());}
+    
+    try {$processOrderbook = processOrderbook($symbol);}
+    catch(Exception $e) {apologize($e->getMessage());} 
+
     redirect("instatrade.php");
 }
 else{
