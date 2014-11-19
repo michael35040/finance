1e4d2034953cec39225014fb07b1174e0b45585b
michael35040 <michaelsjohnson@gmail.com>
2014-11-19 15:01:12 -0500

    Update instatrade.php

--- public_html/instatrade.php
+++ public_html/instatrade.php
@@ -2,6 +2,8 @@
 
 require("../includes/config.php");
 
+ $id = $_SESSION["id"]; //get id from session
+ 
 // if form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
@@ -9,25 +11,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     @$type = $_POST["type"]; //limit or market
     @$side = $_POST["side"]; //buy/bid or sell/ask
     @$quantity = (int)$_POST["quantity"]; //not set on market orders
-    @$price = (float)$_POST["price"]; //not set on market orders
 
     @$metalTransaction = $_POST["metalTransaction"];// buyGold, buySilver, sellGold, sellSilver
-    
-    //apologize(var_dump(get_defined_vars()));
-    
         if($metalTransaction=='buyGold'){$symbol='GOLD';$side='bid';}
         elseif($metalTransaction=='buySilver'){$symbol='SILVER';$side='bid';}
         elseif($metalTransaction=='sellGold'){$symbol='GOLD';$side='ask';}
         elseif($metalTransaction=='sellSilver'){$symbol='SILVER';$side='ask';}
         else{apologize('Unknown action!');}// //dump all variables if i hit error
-    //FORMATS AND SCRUBS VARIABLES
-    $price = sanatize("price", $price);
-    $quantity = sanatize("quantity", $quantity);
-    $symbol = strtoupper($symbol); //cast to UpperCase
+        //apologize(var_dump(get_defined_vars()));
     
-    if($symbol!='GOLD' || $symbol!='SILVER'){apologize('Invalid metal type! Gold or Silver only.');}
+    //FORMATS AND SCRUBS VARIABLES
+    $quantity = sanatize("quantity", $quantity);
     $type = 'market';
-    $side = sanatize("alphabet", $side);
+    $price = 0; //market
+
+    apologize($symbol . " " . $side . " " . $price . " " . $quantity . " " . $type);
+    
     
     try {list($transaction, $symbol, $tradeTotal, $quantity, $commissionTotal) = placeOrder($symbol, $type, $side, $quantity, $price, $id);}
     catch(Exception $e) {apologize($e->getMessage());}
