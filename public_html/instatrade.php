3e26dc291de9bcd72b7e144a24391d3695799b31
michael35040 <michaelsjohnson@gmail.com>
2014-11-19 14:34:20 -0500

    Update instatrade.php

--- public_html/instatrade.php
+++ public_html/instatrade.php
@@ -5,7 +5,23 @@ require("../includes/config.php");
 // if form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-//placeOrder(Market,$symbol,$quantity)
+    @$symbol = $_POST["symbol"]; //assign post variables to local variables, not really needed but makes coding easier
+    @$type = $_POST["type"]; //limit or market
+    @$side = $_POST["side"]; //buy/bid or sell/ask
+    @$quantity = (int)$_POST["quantity"]; //not set on market orders
+    @$price = (float)$_POST["price"]; //not set on market orders
+    //FORMATS AND SCRUBS VARIABLES
+    $price = sanatize("price", $price);
+    $quantity = sanatize("quantity", $quantity);
+    $symbol = strtoupper($symbol); //cast to UpperCase
+    if($symbol!='GOLD' || $symbol!='SILVER'){apologize('Invalid metal type! Gold or Silver only.');}
+    $type = 'market';
+    $side = sanatize("alphabet", $side);
+    try {
+    list($transaction, $symbol, $tradeTotal, $quantity, $commissionTotal) = placeOrder($symbol, $type, $side, $quantity, $price, $id);
+    }
+    catch(Exception $e) {apologize($e->getMessage());}
+    redirect("instatrade.php");
 }
 else{
     render("instatrade_form.php", ["title" => "Instant Trade"]);
