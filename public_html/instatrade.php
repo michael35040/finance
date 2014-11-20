ca5e9c5ae78d2af33772f92a65206ffc69ff5ecc
michael35040 <michaelsjohnson@gmail.com>
2014-11-20 10:03:17 -0500

    Update instatrade.php

--- public_html/instatrade.php
+++ public_html/instatrade.php
@@ -13,57 +13,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     @$quantity = (int)$_POST["quantity"]; //not set on market orders
     $quantity = sanatize("quantity", $quantity);
 
-
-    $goldAmount =	query("SELECT quantity FROM portfolio WHERE id = ? AND symbol='GOLD'", $id);
-    $goldAmount=$goldAmount[0]["quantity"];
-    $goldbids =	query("SELECT price FROM orderbook WHERE (symbol='GOLD' AND side='b' AND type = 'limit') ORDER BY price DESC, uid ASC LIMIT 0, 1");
-    $goldasks =	query("SELECT price FROM orderbook WHERE (symbol='GOLD' AND side='a' AND type = 'limit') ORDER BY price DESC, uid ASC LIMIT 0, 1");
-    $gold["ask"]=(float)$goldasks[0]["price"];
-    $gold["premium"]=($gold["ask"]*0.03);
-    $gold["buy"]=($gold["ask"]+$gold["premium"]);
-    $gold["bid"]=(float)$goldbids[0]["price"];
-    $gold["discount"]=($gold["bid"]*0.03);
-    $gold["sell"]=($gold["bid"]-$gold["discount"]);
-
-    $silverAmount =	query("SELECT quantity FROM portfolio WHERE id = ? AND symbol='SILVER'", $id);
-    $silverAmount=$silverAmount[0]["quantity"];
-    $silverbids =	query("SELECT price FROM orderbook WHERE (symbol='SILVER' AND side='b' AND type = 'limit') ORDER BY price DESC, uid ASC LIMIT 0, 1");
-    $silverasks =	query("SELECT price FROM orderbook WHERE (symbol='SILVER' AND side='a' AND type = 'limit') ORDER BY price DESC, uid ASC LIMIT 0, 1");
-    $silver["ask"]=(float)$silverasks[0]["price"];
-    $silver["premium"]=($silver["ask"]*0.03);
-    $silver["buy"]=($silver["ask"]+$silver["premium"]);
-    $silver["bid"]=(float)$silverbids[0]["price"];
-    $silver["discount"]=($silver["bid"]*0.03);
-    $silver["sell"]=($silver["bid"]-$silver["discount"]);
-
-
     @$metalTransaction = $_POST["metalTransaction"];// buyGold, buySilver, sellGold, sellSilver
         if($metalTransaction=='buyGold')
         {   $symbol='GOLD';
             $side='b';
-            $price=($gold["buy"]*$quantity);
         }
         elseif($metalTransaction=='buySilver')
         {   $symbol='SILVER';
             $side='b';
-            $price=($gold["sell"]*$quantity);
         }
         elseif($metalTransaction=='sellGold')
         {   $symbol='GOLD';
             $side='a';
-            $price=($silver["buy"]*$quantity);
         }
         elseif($metalTransaction=='sellSilver')
         {   $symbol='SILVER';
             $side='a';
-            $price=($silver["sell"]*$quantity);
         }
         else{apologize('Unknown action!');}// //dump all variables if i hit error
         //apologize(var_dump(get_defined_vars()));
     
     //FORMATS AND SCRUBS VARIABLES
     $type = 'market';
-
+    $price=0; //market order, price is meaningless
     //apologize($symbol . " " . $type . " " . $side . "/ x" . $quantity . "/ $" . $price . "/ ID" . $id);
     
     
