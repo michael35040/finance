d04b3e69ed08ae84f90bda33e8d25ca43d950263
michael35040 <michaelsjohnson@gmail.com>
2014-11-21 10:23:19 -0500

    Update index.php

--- public_html/info/index.php
+++ public_html/info/index.php
@@ -1,25 +1,5 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>Pulwar</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-xlarge.css" />
-		</noscript>
-	</head>
-	<body id="top">
-
-		<!-- Header -->
-        <?php include 'header.php'; ?>
+<!-- Header -->
+<?php include 'header.php'; ?>
 
 
 		<!-- Banner -->
