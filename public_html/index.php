c69383dff8fb4987a3c97c007742f703b7523d2b
michael35040 <michaelsjohnson@gmail.com>
2014-11-21 10:10:30 -0500

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,17 @@
 <?php
-require("../includes/config.php");
+
+//NOT USING AS IT HAS ITS OWN AUTHENTICATION, COULD ADD INDEX.php TO IT BUT DON'T REALLY NEED ANYTHING FROM IT.
+//require("../includes/config.php"); // configuration 
+
+//attempting to stop header errors
+ob_start();
+// enable sessions
+session_start();
 
 if(isset($_SESSION["id"]))
+//{ apologize(var_dump(get_defined_vars())); }//dump all variables if i hit error
 { header('Location: accounts.php');}
 else
+//{ apologize(var_dump(get_defined_vars())); }//dump all variables if i hit error
 { header('Location: info/index.php');}
 ?>
