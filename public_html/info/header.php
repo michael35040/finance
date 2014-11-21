2616db13f32bbb77d36aafaf36d26caef45f2f4d
michael35040 <michaelsjohnson@gmail.com>
2014-11-21 10:26:48 -0500

    Update header.php

--- public_html/info/header.php
+++ public_html/info/header.php
@@ -1,3 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Pulwar</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+	<body id="top">
+
 <!-- Header -->
 <header id="header" class="skel-layers-fixed">
     <h1><a href="#">PULWAR</a> <img src="images/logo.png" alt="logo" height="33" width="27"></h1>
