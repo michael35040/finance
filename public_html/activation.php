0875f4f60b9aad8b6b8fdbd390da2b294784d764
michael35040 <michaelsjohnson@gmail.com>
2014-11-21 11:06:08 -0500

    Create activation.php

---
+++ public_html/activation.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+
+?>
+[x] Registered. You have succesfully registered.
+[ ] Activated. Now you must wait for your account to be activated.
+[ ] Funded. Please get ready to fund your account.
+
+
+
