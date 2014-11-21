31ba6be93a46221cf28a6892822ca61b69041fc5
michael35040 <michaelsjohnson@gmail.com>
2014-11-21 10:24:16 -0500

    Update no-sidebar.php

--- public_html/info/no-sidebar.php
+++ public_html/info/no-sidebar.php
@@ -1,30 +1,5 @@
-<!DOCTYPE HTML>
-<!--
-	Ion by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
-<html>
-	<head>
-		<title>No Sidebar - Ion by TEMPLATED</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-xlarge.css" />
-		</noscript>
-	</head>
-	<body id="top">
-
-		<!-- Header -->
-        <?php include 'header.php'; ?>
+<!-- Header -->
+<?php include 'header.php'; ?>
 
 
 		<!-- Main -->
