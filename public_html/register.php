e809d7fe8d6d8310c708f753fbb47d3c07b7e17b
michael35040 <michaelsjohnson@gmail.com>
2014-11-25 15:18:46 -0500

    Update register.php

--- public_html/register.php
+++ public_html/register.php
@@ -71,10 +71,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
  * In this case, we want to increase the default cost for BCRYPT to 12.
  * Note that we also switched to BCRYPT, which will always be 60 characters.
  */
-$options = [
-    'cost' => 12,
-];
-$password = password_hash($password, , PASSWORD_BCRYPT, $options);
+$options = ['cost' => 12,];
+$password = password_hash($password, PASSWORD_BCRYPT, $options);
 
 
 
