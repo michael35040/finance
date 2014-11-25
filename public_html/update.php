57a88da7acca60159cf5d50d779345c8117eaba0
michael35040 <michaelsjohnson@gmail.com>
2014-11-25 13:40:30 -0500

    Update update.php

--- public_html/update.php
+++ public_html/update.php
@@ -10,88 +10,68 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$row = $rows[0];
 	if (count($rows) == 1) // if we found user, check password
 	{
-
-
-
-        $row = $rows[0];  // first (and only) row
+        	$row = $rows[0];  // first (and only) row
 		if (crypt($_POST["password"], $row["password"]) == $row["password"])// compare password of user's input against password that's in database
 		{
+			$fname = $_POST["fname"];
+			$fname = sanatize("alphabet", $fname);
+			$lname = $_POST["lname"];
+			$lname = sanatize("alphabet", $lname);
+			$email = $_POST["email"];
+			$email = sanatize("email", $email);
+			$address = $_POST["address"];
+			$address = sanatize("address", $address);
+			$city = $_POST["city"];
+			$city = sanatize("alphabet", $city);
+			$region = $_POST["region"]; //state
+			$region = sanatize("alphabet", $region);
+			$zip = (int)$_POST["zip"];
+			$zip = sanatize("wholenumber", $zip);
+			$phone = $_POST["phone"];
+			$phone = sanatize("phone", $phone);
+			$answer = $_POST["answer"];
+			$question = $_POST["question"];
+			$password = $_POST["password"];
 
+			if (empty($fname)) { apologize("You must provide a First Name."); }
+			if (empty($lname)) { apologize("You must provide a Last Name."); }
+			if (empty($email)) { apologize("You must provide an Email."); }
+			if (empty($address)) { apologize("You must provide an Address."); }
+			if (empty($city)) { apologize("You must provide a City."); } //not mandatory
+			if (empty($region)) { apologize("You must provide a State/Region."); }
+			if (empty($zip)) { apologize("You must provide a Postal Code."); }
+			if (empty($phone)) { apologize("You must provide a Phone Number."); }
+			if (empty($question)) { apologize("You must provide a Security Question."); }
+			if (empty($answer)) { apologize("You must provide a Security Answer."); }
+			if (empty($password)) { apologize("You must provide a Password."); }
 
-            $fname = $_POST["fname"];
-            $lname = $_POST["lname"];
-            $email = $_POST["email"];
-            $address = $_POST["address"];
-            $city = $_POST["city"];
-            $region = $_POST["region"];
-            $zip = $_POST["zip"];
-            $phone = $_POST["phone"];
-            $question = $_POST["question"];
-            $answer = $_POST["answer"];
+			if (query("
+			UPDATE users 
+			SET email=?, fname=?, lname=?, address=?, city=?, region=?, zip=?, phone=?, question=?, answer=? 
+			WHERE id = ?",
+			$email, $fname, $lname, $address, $city, $region, $zip, $phone, $question, $answer, $_SESSION["id"]) === false)
+			{apologize("Email already in use.");}
 
-
-            
-
-            if (query("
-        INSERT INTO users (email, fname, lname, address, city, region, zip, phone, question, answer, password, registered, last_login, ip, fails, active)
-        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                    $email, $fname, $lname, $address, $city, $region, $zip, $phone, $question, $answer, $password, $now, $now, $ipaddress, 0, 0) === false)
-            {
-                query("ROLLBACK"); //rollback on failure
-                query("SET AUTOCOMMIT=1");
-                apologize("Email already in use. #1");
-            }
-
-
-
-
-            $change = $_POST["change"];
-			if ($change == 'email')
+			if(!empty($_POST["newpassword"]))
 			{
-				if (empty($_POST["email"]) || empty($_POST["confirmation"])){apologize("You must provide a email.");}
-				if ($_POST["email"] != $_POST["confirmation"]){apologize("Missmatch. The new email does not match its confirmation.");}
-				$email = $_POST["email"];
-				$email = sanatize("email", $email);
-				if (query("UPDATE users SET email = ( ? ) WHERE id = ( ? )", ($email), $_SESSION["id"]) === false) {apologize("Sorry, some internal error ocured.");}
-			}//email
-			if ($change == 'phone')
-			{	
-				if (empty($_POST["phone"]) || empty($_POST["confirmation"])){apologize("You must provide a phone.");}
-				if ($_POST["phone"] != $_POST["confirmation"]){ apologize("Missmatch. The new phone does not match its confirmation."); }       
-				$phone = $_POST["phone"];
-				$phone=sanatize("phone", $phone); //functions.php
-				if(query("UPDATE users SET phone = ( ? ) WHERE id = ( ? )", $phone, $_SESSION["id"]) === false){ apologize("Sorry, username already taken."); }
-			}//phone
-			if ($change == 'password')
-			{	
-				if (empty($_POST["password"]))  // validate submission{apologize("You must provide your old password.");}
-				if (empty($_POST["newpassword"]) || empty($_POST["confirmation"])){apologize("You must enter your new password and re-type it.");}          
-				if ($_POST["newpassword"] != $_POST["confirmation"]){apologize("Missmatch. The new password does not match its Confirmation.");}
-				if(query("UPDATE users SET password = ? WHERE id = ? ", generate_hash($_POST["newpassword"]), $_SESSION["id"]) === false) {apologize("Sorry, some internal error ocured.");}
-			}//password
-			redirect("update.php");
-		} //password check is correct
-		else // (crypt($_POST["password"], $row["password"]) != $row["password"])
-			{apologize("Sorry, the 'Current Password' was not correct.");}// compare password of user's input against password that's in database
-	} //row of # users
-	else {apologize("Sorry, user information incorrect.");}//to many rows
+				if (empty($_POST["newpassword"]) || empty($_POST["newconfirmation"])) { apologize("If selecting a new password, re-type it in the new confirmation box."); }
+				if($_POST["newpassword"]==$_POST["newconfirmation"])
+				{
+					$newpassword = $_POST["newpassword"];
+					$newpassword = generate_hash($newpassword); //generate blowfish hash from functions.php
+					if (strlen($password) != 60) { apologize("Invalid password configuration."); } // The hashed pwd should be 60 characters long. If it's not, something really odd has happened
+					query("UPDATE users SET password=?", $newpassword);
+					
+				}
+				else{apologize("New password and new confirmation do not match!");}
+			}
+		}else{apologize("Incorrect Password!");}	
+	}else{apologize("unknown user!");}
+redirect("update.php");
 }  //post
 else{
-	$userinfo = query("SELECT * FROM users WHERE id = ( ? )", $_SESSION["id"]);
+	$userinfo = query("SELECT * FROM users WHERE id =?", $_SESSION["id"]);
 	render("update_form.php", ["title" => "Update", "userinfo" => $userinfo]);}// else render form
 
-
-/*
-$fname = $userinfo[0]["fname"];
-$lname = $userinfo[0]["lname"];
-$email = $userinfo[0]["email"];
-$address = $userinfo[0]["address"];
-$city = $userinfo[0]["city"];
-$region = $userinfo[0]["region"];
-$zip = $userinfo[0]["zip"];
-$phone = $userinfo[0]["phone"];
-$question = $userinfo[0]["question"];
-$answer = $userinfo[0]["answer"];
-*/
 
 ?>
