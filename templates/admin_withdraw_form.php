ad3f54a0d8ee327db055512504b4f01e45d00b1c
michael35040 <michaelsjohnson@gmail.com>
2014-11-28 10:28:19 -0500

    Delete admin_withdraw_form.php

--- templates/admin_withdraw_form.php
+++
@@ -1,9 +0,0 @@
-<form action="admin_withdraw.php" method="post">
-<fieldset>
-<h3>Withdraw:</h3>
-<br /><input class="input-small" name="userid" placeholder="User ID" type="number" min="0" max="any" required />
-<br /><input class="input-medium"  type="number" name="quantity" placeholder="<?php echo($unitsymbol);?> Amount/Quantity" step="0.0000000000001" min="0" max="any" required />
-<br /><button type="submit" class="btn btn-danger"><b>WITHDRAW</b></button>
-</fieldset>
-</form>
-<br /><br />
