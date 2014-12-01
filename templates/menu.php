982d05b59a5893de9aa653418dbecd6bce26d754
michael35040 <michaelsjohnson@gmail.com>
2014-12-01 14:39:05 -0500

    Update menu.php

--- templates/menu.php
+++ templates/menu.php
@@ -5,34 +5,22 @@
             <div class="btn-group">
                 <div class="input-group">
                     <button id="bankButton" type="button" class="btn btn-default  btn-sm   dropdown-toggle" data-toggle="dropdown">
-                        BANKING
+                        <b>MENU</b>
+                    <span class="caret"></span>
                     </button>
 
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="portfolio.php">Portfolio</a></li>
-                        <li><a href="history.php">History</a></li>
-                        <li><a href="update.php">Update</a></li>
-                        <li><a href="status.php">Status</a></li>
-                        <li><a href="assets.php">Assets</a></li>
-                        <li><a href="information.php">Information</a></li>
-
+                        <li><a href="order.php">EXCHANGE</a></li>
+                        <li><a href="portfolio.php">PORTFOLIO</a></li>
+                        <li><a href="orders.php">ORDERS</a></li>
+                        <li><a href="trades.php">TRADES</a></li>
+                        <!--<li><a href="status.php">Status</a></li>-->
+                        <li><a href="assets.php">ASSETS</a></li>
+                        <li><a href="information.php">INFORMATION</a></li>
+                        <li><a href="history.php">HISTORY</a></li>
+                        <li><a href="update.php">UPDATE</a></li>
+                        <!--<li><a href="order2.php">Order 2</a></li>-->
                         <!--<li><a href="transfer.php">Transfer </a></li><li><a href="loan.php">Loan</a></li><?php //if ($loan < 0) { //-0.00000001 ?><li><a href="loanpay.php">Pay Loan</a></li> --><?php //} ?>
-                    </ul>
-                </div>
-            </div>
-
-
-            <div class="btn-group">
-                <div class="input-group">
-                    <button id="exchangeButton" type="button" class="btn btn-default  btn-sm dropdown-toggle" data-toggle="dropdown">
-                        EXCHANGE
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="order.php">Order</a></li>
-                        <li><a href="order2.php">Order 2</a></li>
-                        <li><a href="orders.php">Orders</a></li>
-                        <li><a href="trades.php">Trades</a></li>
-
                     </ul>
                 </div>
             </div>
@@ -60,7 +48,12 @@
 
             <div class="btn-group">
                 <div class="input-group">
-                    <a href="logout.php"><button type="button" class="btn btn-danger  btn-sm ">LOG OUT</button></a>
+                    <a href="logout.php">
+                        <button type="button" class="btn btn-danger  btn-sm ">
+                        <b>EXIT</b>
+                        <span class="glyphicon glyphicon-remove"></span>
+                        </button>
+                    </a>
                 </div>
             </div>
 
