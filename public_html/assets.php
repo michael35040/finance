ccedef60352761886e02334e36b6feadcf9fdab3
michael35040 <michaelsjohnson@gmail.com>
2014-12-01 11:19:33 -0500

    Update assets.php

--- public_html/assets.php
+++ public_html/assets.php
@@ -32,25 +32,25 @@ foreach ($allAssets as $row)		// for each of user's stocks
     $asset["description"] = $row["description"]; //description of asset
         $bid =	query("SELECT price FROM orderbook WHERE (symbol = ? AND side = ? AND type = 'limit') ORDER BY price DESC, uid ASC LIMIT 0, 1", $asset["symbol"], 'b');
         if(empty($bid)){$bid=0;}
-    $asset["bid"] = $bid[0]["price"]; //stock price per share
+    $asset["bid"] = getPrice($bid[0]["price"]); //stock price per share
         $ask =	query("SELECT price FROM orderbook WHERE (symbol = ? AND side = ? AND type = 'limit') ORDER BY price ASC, uid ASC LIMIT 0, 1", $asset["symbol"], 'a');
         if(empty($ask)){$ask=0;}
-    $asset["ask"] = $ask[0]["price"]; //stock price per share
+    $asset["ask"] = getPrice($ask[0]["price"]); //stock price per share
         $volume =	query("SELECT SUM(quantity) AS quantity, AVG(price) AS price, date FROM trades WHERE symbol =? GROUP BY DAY(date) ORDER BY uid ASC ", $asset["symbol"]);	  // query user's portfolio
         if(empty($volume[0]["quantity"])){$volume[0]["quantity"]=0;}
         if(empty($volume[0]["price"])){$volume[0]["price"]=0;}
     $asset["volume"] = $volume[0]["quantity"];
-    $asset["avgprice"] = $volume[0]["price"];
+    $asset["avgprice"] = getPrice($volume[0]["price"]);
         $trades = query("SELECT price FROM trades WHERE (symbol=? AND (type='limit' OR type='market')) ORDER BY uid DESC LIMIT 0,1", $asset["symbol"]);
         //$trades = query("SELECT price FROM trades WHERE symbol = ? ORDER BY uid DESC LIMIT 0, 1", $asset["symbol"]);	  // query user's portfolio
         if(empty($trades[0]["price"])){$trades[0]["price"]=0;}
-    $asset["price"] = $trades[0]["price"]; //stock price per share
-    $asset["marketcap"] = ($asset["price"] * $asset["issued"]);
+    $asset["price"] = getPrice($trades[0]["price"];) //stock price per share
+    $asset["marketcap"] = getPrice($asset["price"] * $asset["issued"]);
         //$dividend =	query("SELECT SUM(quantity) AS quantity FROM history WHERE type = 'dividend' AND symbol = ?", $asset["symbol"]);	  // query user's portfolio
         $asset["dividend"]=0; //until we get real ones
     //$asset["dividend"] = $dividend["dividend"]; //shares actually held public
-    $indexMarketCap = $indexMarketCap+$asset["marketcap"];
-    $indexValue = $indexValue+$asset["price"];
+    $indexMarketCap = getPrice($indexMarketCap+$asset["marketcap"]);
+    $indexValue = getPrice($indexValue+$asset["price"]);
     $assets[] = $asset;
 
     //apologize(var_dump(get_defined_vars()));
