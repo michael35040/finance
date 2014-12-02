080eaa089f46a32d2373c09c83fc4941c9f65dcd
michael35040 <michaelsjohnson@gmail.com>
2014-12-02 15:39:07 -0500

    Update right-sidebar.php

--- public_html/info/right-sidebar.php
+++ public_html/info/right-sidebar.php
@@ -21,8 +21,29 @@
 						</div>
 						<div class="4u">
 							<section>
-								<h3>Magna massa blandit</h3>
-								<p>Feugiat amet accumsan ante aliquet feugiat accumsan. Ante blandit accumsan eu amet tortor non lorem felis semper. Interdum adipiscing orci feugiat penatibus adipiscing col cubilia lorem ipsum dolor sit amet feugiat consequat.</p>
+								<h3>HARD COMMODITIES</h3>
+<p>FeugiatWe specialize in the buy, sell, and trade of hard commodity assets. These include precious metals such as gold and silver. Our exchange allows customers to take a position in this unique asset class. You can buy in amounts as small as one gram and take delivery once acquiring one kilogram (32.151 troy ounces).</p>
+<p>The Good Delivery Rules set out every aspect of the Good Delivery administration including the specification standards for our exchange-traded gold and silver bars.</p>
+<p>The Good Delivery Rules provides information for existing refiners and the banks that work with them, as well as the necessary guidance for refiners seeking accreditation.</p>
+<p>Only bars that meet Good Delivery standards are acceptable in the physical settlement of an exchange gold or silver transaction. The high level of consistency within our market is maintained by ensuring both refiners and vaults implement the Good Delivery Rules relating to a bar's assay, weight and its safe handling and stacking. The main specifications for GD gold and silver bars are summarised below. No other refined gold or silver products produced by accredited refiners fall within the scope of the Good Delivery Lists.</p>
+<p>Specifications for a Good Delivery Silver Bar. 
+	<b>Weight:</b> Minimum silver content: 750 troy ounces (approximately 23 kilograms) maximum silver content: 1100 troy ounces (approximately 34 kilograms) However, it is recommended that ideally refiners should aim to produce bars within the following weight range; Minimum silver content: 900 troy ounces (approximately 29 kilograms); Maximum silver content: 1050 troy ounces (approximately 33 kilograms). The gross weight of a bar should be expressed in troy ounces in multiples of 0.10, rounded down to the nearest 0.10 of a troy ounce.
+	<b>Dimensions:</b> The recommended dimensions for a Good Delivery silver bar are approximately as follows: Length (Top): 300 mm +/- 50 mm Undercut: * 5-15 degrees; Width (Top): 130mm +/- 20 mm Undercut: * 5-15 degrees; Height: 80 mm +/- 20; * The undercut refers to the degree of slope on the side and ends of the bar and is calculated using the following formulae: 90 degrees(ATAN(H/((T-B)* 0.5))), where H=Height, T= Top, and B=Bottom dimensions of the bar, the bottom dimension being measured to the theoretical sharp edges.
+	<b>Fineness:</b> The minimum acceptable fineness is 999.0 parts per thousand silver.
+	<b>Marks:</b> Serial number; Assay stamp of refiner; Fineness (to three significant figures); Year of manufacture.
+</p>
+<p>Specifications for a Good Delivery Gold Bar.
+	<b>Weight:</b> Minimum gold content: 350 fine troy ounces (approximately 10.9 kilograms) maximum gold content: 430 fine troy ounces (approximately 13.4 kilograms) The gross weight of a bar should be expressed in troy ounces, in multiples of 0.025, rounded down to the nearest 0.025 of a troy ounce.
+	<b>Dimensions:</b> The recommended dimensions for a Good Delivery gold bar are approximately as follows:
+	<b>Length (Top):</b> 250 mm+/- 40 mm Undercut * range: 5-25 degrees.
+	<b>Width (Top):</b> 70 mm +/- 15 mm Undercut *: 5-25 degrees.
+	<b>Height:</b> 35 mm +/- 10 mm; * The undercut refers to the degree of slope on the side and ends of the bar and is calculated using the following formulae: 90 degrees(ATAN(H/((T-B)* 0.5))), where H=Height, T= Top, and B=Bottom dimensions of the bar, the bottom dimension being measured to the theoretical sharp edges.
+	<b>Fineness:</b> The minimum acceptable fineness is 995.0 parts per thousand fine gold.
+	<b>Marks:</b> Serial number; Assay stamp of refiner; Fineness (to four significant figures); Year of manufacture.
+</p>
+<p><b>Assays:</b> It is essential that all GD bars contain the amount of metal stated by the marked assay as marked on the bar and its weight. Assays of GD bars are determined by the refiner at the point of manufacture. An assay is likely only to be checked again when a bar is used as raw material in a refinery, for example, for the manufacture of small bars or in a plant manufacturing products like jewellery.</p>
+								
+								
 								<ul class="actions">
 									<li><a href="#" class="button alt">Learn More</a></li>
 								</ul>
