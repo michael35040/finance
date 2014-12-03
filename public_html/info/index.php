336fa863e840a69a083b455048208d3b8f585c51
michael35040 <michaelsjohnson@gmail.com>
2014-12-03 11:16:01 -0500

    Update index.php

--- public_html/info/index.php
+++ public_html/info/index.php
@@ -1,8 +1,6 @@
 <!-- Header -->
 <?php 
 
-$sitename="Pulwar";
-$logo="Financial Excellence";
 
 include 'header.php'; 
 
@@ -24,7 +22,7 @@ include 'header.php';
 		<!-- One -->
 			<section id="one" class="wrapper style1">
 				<header class="major">
-					<h2>Solutions</h2>
+					<h2>SOLUTION ORIENTED</h2>
 					<p>We offer a unique set of capabilities to solve financial issues</p>
 				</header>
 				<div class="container">
@@ -32,22 +30,27 @@ include 'header.php';
 						<div class="4u">
 							<section class="special box">
 								<i class="icon fa-area-chart major"></i>
-								<h3>INVESTMENT BANKING</h3>
-								<p>We provide a broad range of investment banking services to a diverse group of corporations, financial institutions, investment funds and governments. Services include strategic advisory assignments with respect to mergers and acquisitions, divestitures, corporate defense activities, risk management, restructurings and spin-offs, and debt and equity underwriting of public offerings and private placements, including domestic and cross-border transactions, as well as derivative transactions directly related to these activities.</p>
+								<h3>INVESTING</h3>
+								<p>Our transparent pricing model with no hidden fees and no middlemen allow for the lowest prices.</p>
+								<!--
+								-->
 							</section>
 						</div>
 						<div class="4u">
 							<section class="special box">
 								<i class="icon fa-refresh major"></i>
-								<h3>INSTITUTIONAL CLIENT SERVICES</h3>
-								<p>We facilitate client transactions and make markets in fixed income, equity, currency and commodity products, primarily with institutional clients such as corporations, financial institutions, investment funds and governments. We also make markets in and clear client transactions on major stock, options and futures exchanges worldwide and provide financing, securities lending and other prime brokerage services to institutional clients.</p>
+								<h3>TRADING</h3>
+								<p>Instantly buy and sell various assets using an innovative platform created by investors, for investors, with the latest technology available.</p>
+								<!--
+								-->
 							</section>
 						</div>
 						<div class="4u">
 							<section class="special box">
 								<i class="icon fa-cog major"></i>
-								<h3>INVESTING & LENDING</h3>
-								<p>We invest in and originate loans to provide financing to clients. These investments and loans are typically longer term in nature. We make investments, some of which are consolidated, directly and indirectly through funds that we manage, in debt securities and loans, public and private equity securities, and real estate entities.</p>
+								<h3>SERVICES</h3>
+								<p>We offer unique set of services ranging from advisory, management, and conseling to secure depository storage.</p>
+								
 							</section>
 						</div>
 					</div>
@@ -65,8 +68,19 @@ include 'header.php';
 						<div class="6u">
 							<section class="special">
 								<a href="#" class="image fit"><img src="images/pic01.jpg" alt="" /></a>
-								<h3>PRODUCTS</h3>
-								<p>We provide investment management services and offer investment products (primarily through separately managed accounts and commingled vehicles, such as mutual funds and private investment funds) across all major asset classes to a diverse set of institutional and individual clients. </p>
+							
+							<h3>PRODUCTS</h3>
+								<p>We offer exclusive research and analysis products on a wide range of topics. Ranging from
+								business competitiveness to equity forcasting, these products help our customers to deal with 
+								their financial horizons. Whether the problem is a business needing an analysis of their competitors 
+								process and products in comparison to their own or an investor needs an in-depth earnings forecast 
+								to assist in planning their future market posistions, we have the solution.</p>
+								
+								<p>We offer a vareity of products for direct sale purchasing. We specialize in 
+								assisting our customers invest in hard asset commodities. What differentiates these offerings from those
+								available on the exchange are that these are available for immediate shipping.</p>
+
+
 								<ul class="actions">
 									<li><a href="../register.php" class="button alt">Learn More</a></li>
 								</ul>
@@ -75,8 +89,37 @@ include 'header.php';
 						<div class="6u">
 							<section class="special">
 								<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
-								<h3>SERVICES</h3>
-								<p>We also offer wealth advisory services, including portfolio management and financial counseling, and brokerage and other transaction services to high-net-worth individuals and families.</p>
+							
+							<h3>SERVICES</h3>
+								
+								<p>We also offer wealth advisory services, including portfolio management and financial counseling, 
+								and brokerage and other transaction services to high-net-worth individuals and families.</p>
+								
+								<p>We provide a broad range of investment banking services to a diverse group of corporations, 
+								financial institutions, investment funds and governments. Services include strategic advisory 
+								assignments with respect to mergers and acquisitions, divestitures, corporate defense activities, 
+								risk management, restructurings and spin-offs, and debt and equity underwriting of public offerings 
+								and private placements, including domestic and cross-border transactions, as well as derivative 
+								transactions directly related to these activities.</p>
+								
+								<p>We facilitate client transactions and make markets in fixed income, equity, currency and commodity 
+								products, primarily with institutional clients such as corporations, financial institutions, investment 
+								funds and governments. We also make markets in and clear client transactions on major stock, options 
+								and futures exchanges worldwide and provide financing, securities lending and other prime brokerage 
+								services to institutional clients.</p>
+							
+								<p>We have parterned with secure storage facilities so our customers can inexpensively secure their 
+								assets in either allocated or unallocated accounts.</p>
+								
+								<p>We provide investment management services and offer investment products (primarily through separately 
+								managed accounts and commingled vehicles, such as mutual funds and private investment funds) across all 
+								major asset classes to a diverse set of institutional and individual clients. </p>
+								
+								<p>We invest in and originate loans to provide financing to clients. These investments and loans are 
+								typically longer term in nature. We make investments, some of which are consolidated, directly and 
+								indirectly through funds that we manage, in debt securities and loans, public and private equity 
+								securities, and real estate entities.</p>
+
 								<ul class="actions">
 									<li><a href="../register.php" class="button alt">Learn More</a></li>
 								</ul>
@@ -92,15 +135,15 @@ include 'header.php';
 					<div class="row">
 						<div class="8u">
 							<section>
-								<h2>EXCHANGE</h2>
+								<h2>How to start!</h2>
 								<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
-								<p>
-Before you begin buying and selling on the exchange you will need to fund your account. 
-Please be sure to read and understand the specifics of each account funding option before making the choice that works best for you. 
-Certain methods of funding your account require longer processing times. 
-We have three methods available for depositing funds: Wire Transfer, ACH Transfer, and Check. 
-Due to the non-immediate nature of debit and credit account transfers, as well as the commissions attached to debit and credit of transactions, we only offer trades to accounts funded by the methods listed.
-Here are the full details on how to add funds to your account, and our customer service team is available to help if you have any questions.<br>	
+<p>
+	Before you begin buying and selling on the exchange you will need to fund your account. 
+	Please be sure to read and understand the specifics of each account funding option before making the choice that works best for you. 
+	Certain methods of funding your account require longer processing times. 
+	We have three methods available for depositing funds: Wire Transfer, ACH Transfer, and Check. 
+	Due to the non-immediate nature of debit and credit account transfers, as well as the commissions attached to debit and credit of transactions, we only offer trades to accounts funded by the methods listed.
+	Here are the full details on how to add funds to your account, and our customer service team is available to help if you have any questions.<br>	
                             <ul>
                                     <li>
                                         <b>Bitcoin</b> is the fastest method of funding your account. A bitcoin transaction takes up to 30 minutes to verify and to post to your account.<br>
@@ -116,23 +159,23 @@ Here are the full details on how to add funds to your account, and our customer 
                                     </li>
 
                             </ul>
-
-								</p>
+</p>
 							</section>
 						</div>
 						<div class="4u">
 							<section>
-								<h3>ORDERS</h3>
-								<p>
-Our local exchange provides the ability to trade equities and commodities. 
-At the moment there are two types of orders: LIMIT and MARKET orders. 
-Both can be used to enter or exit the market. 
-LIMIT orders are only traded once the trade price matches or exceeds the order price. 
-MARKET orders are executed immediately and assigned to the best BID or the best ASK. 
-The order entry ticket is simple yet versatile. 
-Open orders can be canceled if they have not yet been matched and a trade executed.
+<h3>ORDERS</h3>
+<p>
+	Our local exchange provides the ability to trade equities and commodities. 
+	At the moment there are two types of orders: LIMIT and MARKET orders. 
+	Both can be used to enter or exit the market. 
+	LIMIT orders are only traded once the trade price matches or exceeds the order price. 
+	MARKET orders are executed immediately and assigned to the best BID or the best ASK. 
+	The order entry ticket is simple yet versatile. 
+	Open orders can be canceled if they have not yet been matched and a trade executed.
 
-								</p>
+</p>
+
 								<ul class="actions">
 									<li><a href="../register.php" class="button alt">Learn More</a></li>
 								</ul>
