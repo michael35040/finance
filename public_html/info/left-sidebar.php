277c3834a4dc684baa2c13b38938041720b8cd0d
michael35040 <michaelsjohnson@gmail.com>
2014-12-03 14:52:17 -0500

    Update left-sidebar.php

--- public_html/info/left-sidebar.php
+++ public_html/info/left-sidebar.php
@@ -35,6 +35,30 @@
 								<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
 								<p>Pulwar accepts deposits, lends to individuals and corporate clients, and transacts in derivatives (including interest rate, credit and foreign currency products).</p>
 								<p>Pulwar deploys capital to help transform distressed communities into sustainable and vibrant neighborhoods of choice and opportunity. GS Bank is committed to helping communities through the work of the Urban Investment Group which provides community development lending, investing and grant making with a focus on renewal.</p>
+								<p>
+FUNDING
+	Before you begin buying and selling on the exchange you will need to fund your account. 
+	Please be sure to read and understand the specifics of each account funding option before making the choice that works best for you. 
+	Certain methods of funding your account require longer processing times. 
+	We have three methods available for depositing funds: Wire Transfer, ACH Transfer, and Check. 
+	Due to the non-immediate nature of debit and credit account transfers, as well as the commissions attached to debit and credit of transactions, we only offer trades to accounts funded by the methods listed.
+	Here are the full details on how to add funds to your account, and our customer service team is available to help if you have any questions.<br>	
+                            <ul>
+                                    <li>
+                                        <b>Bitcoin</b> is the fastest method of funding your account. A bitcoin transaction takes up to 30 minutes to verify and to post to your account.<br>
+                                    </li>
+                                    <li>
+                                        <b>Wire Transfer</b> is the second fastest method for funding your account. Wire transfers generally execute the same day, but may take up to one additional business day to post to your account. Your bank may charge a wire fee.<br>
+                                    </li>
+                                    <li>
+                                        <b>ACH (Automated Clearing House) Transfer</b> generally take two to three business days to post to your account. However, the fees to send this type of transfer are normally more affordable than wire fees.<br>
+                                    </li>
+                                    <li>
+                                        <b>Check</b> is the slowest method of funding your account, but also the least expensive. A bank check takes up to 14 business days to post to your account after it is received and processed.<br>
+                                    </li>
+
+                            </ul>
+</p>
 							</section>
 						</div>
 					</div>
