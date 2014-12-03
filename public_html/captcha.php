2d225057b1c494bb882dad27da044f987e12047a
michael35040 <michaelsjohnson@gmail.com>
2014-12-03 10:26:09 -0500

    Update captcha.php

--- public_html/captcha.php
+++ public_html/captcha.php
@@ -3,7 +3,8 @@ session_start();
 $code=rand(1000,9999);
 $_SESSION["code"]=$code;
 $im = imagecreatetruecolor(50, 24);
-$bg = imagecolorallocate($im, 22, 86, 165); //background color blue
+$bg = imagecolorallocate($im, 255, 0, 0); //background color blue
+//$bg = imagecolorallocate($im, 22, 86, 165); //background color blue
 $fg = imagecolorallocate($im, 255, 255, 255);//text color white
 imagefill($im, 0, 0, $bg);
 imagestring($im, 5, 5, 5,  $code, $fg);
