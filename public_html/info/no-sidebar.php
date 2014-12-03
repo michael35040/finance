9c9d6f9990b84f7946a16d05cedd2e0be86ac54d
michael35040 <michaelsjohnson@gmail.com>
2014-12-03 11:14:58 -0500

    Update no-sidebar.php

--- public_html/info/no-sidebar.php
+++ public_html/info/no-sidebar.php
@@ -16,6 +16,38 @@
 						<a href="#" class="image fit"><img src="images/banner.jpg" alt="" /></a>
 						<p>Vis accumsan feugiat adipiscing nisl amet adipiscing accumsan blandit accumsan sapien blandit ac amet faucibus aliquet placerat commodo. Interdum ante aliquet commodo accumsan vis phasellus adipiscing. Ornare a in lacinia. Vestibulum accumsan ac metus massa tempor. Accumsan in lacinia ornare massa amet. Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.</p>
 						<p>Amet nibh adipiscing adipiscing. Commodo ante vis placerat interdum massa massa primis. Tempus condimentum tempus non ac varius cubilia adipiscing placerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet blandit adipiscing arcu ante.</p>
+<p><b>Open an account. We’ve made our registration process as simple and efficient as possible. Sign up, verify, and fund your account, then start trading as soon as your registration is processed.<b>
+At the beginning of the registration process, you will need to create your personal login and password. This login and password will be required for you to gain access to the trading portal, dashboard and other pages reserved for registered customers only.
+You will be asked to enter some basic information – your name, your email address, a few security questions, and some other vitals. This is also the page where you will create your user password. This is the first step in the registration process.
+Complete this section and then click the “submit” button. You’ll be prompted if any information is missing or incorrectly formatted. If you are not able to successfully complete this registration step, or have any questions at all , feel free to contact our customer service department. Otherwise, once you hit submit, you’ll move on to the second step, identity verification.
+</p>
+
+<p>Verify your identity. Security is our highest priority, which is why we verify the identity of every user before granting them full access to the bullion exchange. This verification process also allows us to identify you as the legal owner of your metals and helps to keep your account secure.
+You’ll be asked to email or fax a copy of your valid government-issued ID along with a legible bank statement or voided check. If you are not sure whether a form of identification is acceptable, or have any questions at all regarding account verification, feel free to contact our customer service department at (844) 435-9879.
+We will complete your verification as quickly as possible, please be aware that your documents must be evaluated and processed by our staff before you can begin trading. We will contact you if there is an issue with your verification. Otherwise, you’ll be notified by e-mail when you are ready to fund your account and begin trading. Most verifications take only a few business days.
+
+<p><b>Fund an account. Choose the method that works best for you! We offer three convenient options for funding your account including bank check, automated clearing house, and wire transfer.</b>
+Before you begin buying and selling gold and silver on the bullion exchange you’ll also need to fund your account. Please be sure to read and understand the specifics of each account funding option before making the choice that works best for you. Certain methods of funding your account require longer processing times.
+There are three account funding options available:
+Federal Bank Wire: This is the fastest method for funding your account. Wire transfers generally execute the same day, but may take up to one additional business day to post to your account. Your bank may charge a wire fee.
+ACH Transfer: ACH (Automated Clearing House) transfers generally take two to three business days to post to your account. However, the fees to send this type of transfer are normally more affordable than wire fees.
+Bank Check: This is the slowest method of funding your account, but also the least expensive. A bank check takes up to 14 business days to post to your account after it is received and processed.
+</p>
+
+<p><b>Place a trade. Our straightforward, one-click buy and sell process allows you to execute real-time trades. Buy and sell both gold and silver on the same screen using our easy-to-use interface.</b>
+Time to start trading! Once your identity and account information have been verified and the account has been successfully funded, it’s time to get started on the exchange. If you’d like to learn more about trading, our policies, and how your precious metals are stored, you can view more guides on the menu above under “How it Works.”
+</p>
+
+<p><b>Take delivery. It’s your bullion, and you can take possession of physical metals or withdraw funds anytime you like! Simply request delivery of OPM bullion products, or shop at Provident Metals.</b>
+</p>
+
+<p><b>Secure storage. Protect your allocated bullion with the world’s most advanced technology and infrastructure. We use secure storage providers that have been trusted worldwide for generations.</b>
+</p>
+								
+								
+								
+								
+					
 					</section>
 					<hr class="major" />
 					<div class="row">
