f54c9b873b9c13024395a5a957d413760091d992
michael35040 <michaelsjohnson@gmail.com>
2014-12-04 15:42:01 -0500

    Update information.php

--- public_html/information.php
+++ public_html/information.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     if (empty($_POST["symbol"])) { apologize("Please select a symbol!"); } //check to see if empty
     $symbol = $_POST["symbol"];
-    $symbol = sanatize("symbol", $symbol);
+    $symbol = sanatize("alnum", $symbol);
 
     //COMPANY INFORMATION
     $asset=[];
