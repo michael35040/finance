8b579bb0b1c3422dad200d87d5af7c4f7b0756f3
michael35040 <michaelsjohnson@gmail.com>
2014-12-04 16:07:29 -0500

    Update orders.php

--- public_html/orders.php
+++ public_html/orders.php
@@ -5,9 +5,27 @@ $id =  $_SESSION["id"];
 $title = "Orders";
 $limit = "LIMIT 0, 10";
 $tabletitle = "Last 10";
+$option = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
 {
+    
+    if (isset($_POST["symbol"]))
+    {
+        $symbol = htmlspecialchars($_POST["symbol"]);
+        if (!ctype_alnum($symbol)){apologize("Invalid query!");}
+        $option = 'AND symbol = ' + $symbol;
+        $limit = ""; $tabletitle = htmlspecialchars($symbol);
+    }
+    
+    
+    if (isset($_POST["side"]))
+    {
+        if($_POST["side"]=='b'){$option = "AND side = 'b'";}
+        else{$option = "AND side = 'a'";}
+    }
+    
+    
     if (isset($_POST["cancel"]))
     {
         $uid = $_POST["cancel"];
@@ -23,11 +41,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
             
         }
     }
+    
+    
     if (isset($_POST["history"])) 
     {
         $history = $_POST["history"];
         if ($history == "all") {$limit = ""; $tabletitle = "All";}
     }
+    
 }
 /*
 else
@@ -35,9 +56,9 @@ else
 
 } //else !post , 
 */
-$orders = query("SELECT uid, date, symbol, side, type, quantity, price, total FROM orderbook WHERE (id = ?) ORDER BY uid DESC", $id);
-$ordertotal = query("SELECT SUM(total) AS sumtotal FROM orderbook WHERE (id = ?)", $id);
-$history = query("SELECT ouid, date, symbol, transaction, total FROM history WHERE id = ? ORDER BY uid DESC $limit", $id);
+$orders = query("SELECT uid, date, symbol, side, type, quantity, price, total FROM orderbook WHERE (id = ? $option) ORDER BY uid DESC", $id);
+$ordertotal = query("SELECT SUM(total) AS sumtotal FROM orderbook WHERE (id = ? $option)", $id);
+$history = query("SELECT ouid, date, symbol, transaction, total FROM history WHERE (id = ? $option) ORDER BY uid DESC $limit", $id);
 render("orders_form.php", ["title" => $title, "tabletitle" => $tabletitle, "orders" => $orders,  "ordertotal" => $ordertotal, "history" => $history]);
 
 ?>
