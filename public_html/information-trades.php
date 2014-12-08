8f0f673c51449ffecea6c013d5a664e6317cae55
michael35040 <michaelsjohnson@gmail.com>
2014-12-08 13:57:49 -0500

    Create information-trades.php

---
+++ public_html/information-trades.php
@@ -0,0 +1,80 @@
+<?php
+require("../includes/config.php");
+// if form was submitted
+$title = "Trades";
+$id = $_SESSION["id"];
+
+$symbol='A';
+
+$bids =	query("SELECT * FROM orderbook WHERE (symbol = ? AND side = ? AND type = 'limit') ORDER BY price ASC, uid DESC", $symbol, 'b');
+$asks =	query("SELECT * FROM orderbook WHERE (symbol = ? AND side = ? AND type = 'limit') ORDER BY price ASC, uid ASC", $symbol, 'a');
+
+
+
+echo("<h3 style='text-align:center;'>" . htmlspecialchars(strtoupper($symbol)) . "<br>Trades<br>" . date('l jS \of F Y h:i:s A') . "</h3>");
+
+?>
+
+<style>
+    table
+    {
+        border: 1px solid black;
+    }
+    td
+    {
+        border: 1px solid black;
+    }
+    
+</style>
+
+<table align="center">
+
+<tr>
+    <td>Order #</td>
+    <td>Date/Time (Y/M/D)</td>
+    <td>Price</td>
+    <td>Quantity</td>
+    <td>Commission</td>
+    <td>Total</td>
+    <td>Type</td>
+    <td>Buyer</td>
+    <td>Seller</td>
+    <td>Ask UID</td>
+    <td>Bid UID</td>
+</tr>
+
+
+
+<?php
+foreach ($bids as $row)
+{ 
+if($row['buyer']==$id || $row['seller']==$id){$color=' style="background-color:red"';}
+else{$color='';}
+?> 
+<tr <?php echo($color); ?> >
+    <?php
+
+    echo("<td>" . (number_format($row["uid"],0,".",",")) . "</td>");
+    echo("<td>" . htmlspecialchars(date('Y-m-d H:i:s',strtotime($row["date"]))) . "</td>");
+    echo("<td>" . (number_format(getPrice($row["price"]),2,".",",")) . "</td>");
+    echo("<td>" . (number_format(($row["quantity"]),0,".",",")) . "</td>");
+    echo("<td>" . (number_format(getPrice($row["commission"]),2,".",",")) . "</td>");
+    echo("<td>" . (number_format(getPrice($row["total"]),2,".",",")) . "</td>");
+    echo("<td>" . htmlspecialchars($row["type"]) . "</td>");
+    echo("<td>" . (number_format(($row["buyer"]),0,".",",")) . "</td>");
+    echo("<td>" . (number_format(($row["seller"]),0,".",",")) . "</td>");
+    echo("<td>" . (number_format(($row["askorderuid"]),0,".",",")) . "</td>");
+    echo("<td>" . (number_format(($row["bidorderuid"]),0,".",",")) . "</td>");
+
+    }
+    ?>
+    <td colspan="7"></td>
+</tr>
+
+
+
+
+
+
+
+</table>
