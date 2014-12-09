8254225e52e7bfad5f590e42174a5aca8d92a122
michael35040 <michaelsjohnson@gmail.com>
2014-12-08 21:20:14 -0500

    a

--- public_html/information-trades.php
+++ public_html/information-trades.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     if(isset($_POST['symbol'])){ $symbol=$_POST['symbol'];} 
     else{apologize("Unknown symbol!"); exit();}
     
-    $trades =	query("SELECT * FROM trades WHERE (symbol = ?) ORDER BY date ASC", $symbol);
+    $trades =	query("SELECT * FROM trades WHERE (symbol = ? AND (type='limit' OR type='market')) ORDER BY date ASC", $symbol);
     $tradestotal =	query("SELECT AVG(price) AS avgprice, SUM(total) AS totaltotal, SUM(quantity) AS totalquantity, SUM(commission) AS totalcommission, COUNT(uid) AS totaltrades FROM trades WHERE (symbol = ?) ORDER BY date ASC", $symbol);
     
     render("information-trades_form.php",[
