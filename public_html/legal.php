3149b1b357b59e10f3e829f85b240db15d872d8e
michael35040 <michaelsjohnson@gmail.com>
2014-12-09 11:31:20 -0500

    Create legal.php

---
+++ public_html/legal.php
@@ -0,0 +1,22 @@
+
+
+
+<br>
+<h6>
+ <?php echo(htmlspecialchars($sitename))?> makes no investment recommendations and does not provide financial, tax or legal 
+ advice. Any symbols displayed are for illustrative purposes only and are not intended to portray a recommendation to buy 
+ or sell a particular asset. Trading has inherent risk. System response and access times may vary due to market conditions, 
+ system performance, volume and other factors. These assets involve risk and are not suitable for all individuals. Please 
+ understand these and additional risks before trading.
+<br><br>
+ <?php echo(htmlspecialchars($sitename))?> does not solicit individuals or entities directly or indirectly as clients. 
+ By visiting our site you agree that you have not been solicited by <?php echo(htmlspecialchars($sitename))?> or any 
+ of its officers, directors, managers, owners, agents or representatives to do business with <?php echo(htmlspecialchars($sitename))?>
+ or to purchase or consider the purchase of any assets; that you hereby waive and forever abandon any right or remedy 
+ against <?php echo(htmlspecialchars($sitename))?> and/or any of its officers, directors, managers, owners, agents or 
+ representatives under law. If you do not agree leave this website immediately. The Information is not directed at any 
+ person in any jurisdiction where it is illegal or unlawful to access and use such information. <?php echo(htmlspecialchars($sitename))?> 
+ disclaims all responsibility if you access or download any information in breach of any law or regulation of the country 
+ in which you reside. Accordingly, all persons who access this site are required to inform themselves of and to comply 
+ with all applicable restrictions in their home country. 
+</h6>
