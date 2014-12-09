a09a15d15062f20816159035c91a75076e950435
michael35040 <michaelsjohnson@gmail.com>
2014-12-09 09:15:47 -0500

    Update information-orderbook.php

--- public_html/information-orderbook.php
+++ public_html/information-orderbook.php
@@ -1,7 +1,7 @@
 <?php
 require("../includes/config.php");
 // if form was submitted
-$title = "Trades";
+
 $id = $_SESSION["id"];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
@@ -9,6 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     if(isset($_POST['symbol'])){ $symbol=$_POST['symbol'];}
     else{apologize("Unknown symbol!"); exit();}
 
+    $title = $symbol . " (Orderbook)";
     $bids =	query("SELECT * FROM orderbook WHERE (symbol = ? AND side = ? AND type = 'limit') ORDER BY price ASC, uid DESC", $symbol, 'b');
     $asks =	query("SELECT * FROM orderbook WHERE (symbol = ? AND side = ? AND type = 'limit') ORDER BY price ASC, uid ASC", $symbol, 'a');
 
