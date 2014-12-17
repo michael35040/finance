e8cb7d30541ba7d4faacdc004d17deab91876c8d
michael35040 <michaelsjohnson@gmail.com>
2014-12-17 11:02:35 -0500

    Update convert.php

--- public_html/convert.php
+++ public_html/convert.php
@@ -1,4 +1,5 @@
 <?php
+
 require("../includes/config.php");
 //apologize(var_dump(get_defined_vars()));
 $id = $_SESSION["id"];
@@ -7,26 +8,27 @@ $title = "Convert";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
 {
-    @$symbol1 = $_POST["symbol1"];    //assign post variables to local variables, not really needed but makes coding easier
-    @$symbol2 = $_POST["symbol2"];    //assign post variables to local variables, not really needed but makes coding easier
-    @$amount = (int)$_POST["quantity"]; //not set on market orders
+    if(empty($symbol1) || empty($symbol2) || empty($amount)){apologize("Please fill out all required fields");}
+    if(!is_numeric($amount)){apologize("Invalid Quantity");}
+    
+    $symbol1 = $_POST["symbol1"];    
+    $symbol2 = $_POST["symbol2"];    
+    $amount = (int)$_POST["quantity"]; 
 
     convertAsset($id, $symbol1, $symbol2, $amount);
     redirect("account.php");
 }
 else
 {
+    $assets =	query("SELECT symbol FROM assets ORDER BY symbol ASC");	
 
-    $assets =	query("SELECT symbol FROM assets ORDER BY symbol ASC");	  // query user's portfolio
-
-    $stocksQ = query("SELECT symbol, quantity FROM portfolio WHERE id = ? ORDER BY symbol ASC", $id);	  // query user's portfolio
-    $stocks = []; //to send to next page
-    foreach ($stocksQ as $row)		// for each of user's stocks
-    {   $stock = [];
-        $stock["symbol"] = $row["symbol"]; //set variable from stock info
+    $stocksQ = query("SELECT symbol, quantity FROM portfolio WHERE id = ? ORDER BY symbol ASC", $id);	 
+    foreach ($stocksQ as $row)	
+    {
+        $stock["symbol"] = $row["symbol"]; 
         $stock["quantity"] = $row["quantity"];
-        $askQuantity =	query("SELECT SUM(quantity) AS quantity FROM orderbook WHERE (id=? AND symbol =? AND side='a')", $id, $stock["symbol"]);	  // query user's portfolio
-        $askQuantity = $askQuantity[0]["quantity"]; //shares trading
+        $askQuantity =	query("SELECT SUM(quantity) AS quantity FROM orderbook WHERE (id=? AND symbol=? AND side='a')", $id, $stock["symbol"]);	
+        $askQuantity = $askQuantity[0]["quantity"]; 
         $stock["locked"] = (int)$askQuantity;
         $stocks[] = $stock;
     }
@@ -38,4 +40,4 @@ else
     ]);
 
 }
-
+?>
