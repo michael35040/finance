47ea7804b10b8918a634118e6335aeb2d3faab00
michael35040 <michaelsjohnson@gmail.com>
2014-12-17 11:15:32 -0500

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,4 @@
-<?php
-require("../includes/constants.php"); //global finance constants
-?>
 </div> <!--middle -->
-
-
 <div id="bottom"  style="text-shadow: 1px 1px 5px #000;">
 <br>
  <?php echo(htmlspecialchars($sitename))?> &#169; 1985-2014
