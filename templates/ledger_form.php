90ffe483a013e4d62bd461f033d3f044b4dd15cc
michael35040 <michaelsjohnson@gmail.com>
2015-02-11 13:35:58 -0500

    Update ledger_form.php

--- templates/ledger_form.php
+++ templates/ledger_form.php
@@ -41,19 +41,22 @@
 
         <th>UID</th>
         <th>Date/Time (Y/M/D)</th>
-        <th>Category</th>
+
         <th>User</th>
         <th>Symbol</th>
         <th>Amount</th>
-        <th>Reference</th>
+
         <th>X-User</th>
         <th>X-Symbol</th>
         <th>X-Amount</th>
-        <th>X-Reference</th>
-        <th>Status</th>
+        
+        <th>Category</th>
         <th>Note</th>
         <th>Ask</th>
         <th>Bid</th>
+        <th>Reference</th>
+<!--        <th>X-Reference</th>-->
+        <th>Status</th>
     </tr>
 
     <?php
@@ -65,21 +68,24 @@
         echo("<tr>");
         echo("<td>" . htmlspecialchars($row["uid"]) . "</td>");
         echo("<td>" . htmlspecialchars(date('Y-m-d H:i:s',strtotime($row["date"]))) . "</td>");
-        echo("<td>" . htmlspecialchars($row["category"]) . "</td>");
-        echo("<td>" . htmlspecialchars($row["user"]) . "</td>");
-        echo("<td>" . htmlspecialchars(strtoupper($row["symbol"])) . "</td>");
-        if($row["symbol"]==$unittype){echo("<td>" . $unitsymbol . (number_format(getPrice($row["amount"]),$decimalplaces,".",",")) . "</td>");}
-        else{echo("<td>" . (number_format(($row["amount"]),0,".",",")) . "</td>");}
-        echo("<td>" . htmlspecialchars($row["reference"]) . "</td>");
+
+        echo("<td><b>" . htmlspecialchars($row["user"]) . "</b></td>");
+        echo("<td><b>" . htmlspecialchars(strtoupper($row["symbol"])) . "</b></td>");
+        if($row["symbol"]==$unittype){echo("<td><b>" . $unitsymbol . (number_format(getPrice($row["amount"]),$decimalplaces,".",",")) . "</b></td>");}
+        else{echo("<td><b>" . (number_format(($row["amount"]),0,".",",")) . "</b></td>");}
+
         echo("<td>" . htmlspecialchars($row["xuser"]) . "</td>");
         echo("<td>" . htmlspecialchars(strtoupper($row["xsymbol"])) . "</td>");
         if($row["symbol"]==$unittype){echo("<td>" . $unitsymbol . (number_format(getPrice($row["xamount"]),$decimalplaces,".",",")) . "</td>");}
         else{echo("<td>" . (number_format(($row["xamount"]),0,".",",")) . "</td>");}
-        echo("<td>" . htmlspecialchars($row["xreference"]) . "</td>");
-        echo("<td>" . htmlspecialchars($row["status"]) . "</td>");
+
+        echo("<td>" . htmlspecialchars($row["category"]) . "</td>");
         echo("<td>" . htmlspecialchars($row["note"]) . "</td>");
         echo("<td>" . htmlspecialchars($row["askuid"]) . "</td>");
         echo("<td>" . htmlspecialchars($row["biduid"]) . "</td>");
+        echo("<td>" . htmlspecialchars($row["reference"]) . "</td>");
+//        echo("<td>" . htmlspecialchars($row["xreference"]) . "</td>");
+        echo("<td>" . htmlspecialchars($row["status"]) . "</td>");
         echo("</tr>");
     }
     if($ledger==null){echo('<td colspan="13">None</td>');}
