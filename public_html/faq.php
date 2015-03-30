cf3f781552858fb157042d9a7cba5949d2aa2830
michael35040 <michaelsjohnson@gmail.com>
2015-03-30 09:11:22 -0400

    Update faq.php

--- public_html/faq.php
+++ public_html/faq.php
@@ -4,3 +4,44 @@ An electronic list of buy and sell orders for a specific security or financial i
 by price level. The order book lists the number of shares being bid or offered at each price point, 
 or market depth. It also identifies the market participants behind the buy and sell orders. The order 
 book is dynamic and constantly updated in real time throughout the day. 
+
+Why isn't *this* type of coin/round listed on the exchange?
+Liquidity. If there is enough supply and demand for a coin/round to trade exclusively, it will be listed. Otherwise, 
+these are best traded in the 'pool' categories which is a catch all for all the different types of bullion. We 
+combine all non-specifically-listed types into the groups to increase the amount of buyers and sellers. If a 
+specific group gets enough volume, we will split up the group into their specific types.
+
+What about numismatic premium?
+This exchange is based on trading silver with the lowest overhead. It is about getting the highest price for the
+seller and the lowest price for the buyer by removing steep buy-sell spreads, high commissions, and reducing all
+of the other associated costs when trading physical silver. If you have a high-priced numismatic piece, you are
+best to sell it exclusively on many of the other sites that lets you list an item (eBay, Craigslist, etc.).
+
+Why do you not take Credit Cards, PayPal, etc.?
+This is part of reducing the costs of doing business to provide the lowest overhead. Due to the amount of fraud
+that exists when dealing with credit cards (charge backs, etc.) and our unwillingness to pass costs back to our 
+customers, we have decided to eliminate that risk. With the crypto-currency becoming more common place, it is far
+easier to now conduct transactions via this medium. We recommend Coinbase.com due to the large entities that back
+them (NYSE, USAA, etc.).
+
+Why not just sale all of my bullion at eBay, Craiglist, or the Local Coin Store (LCS)?
+You can. But we are here to provide a marketplace with low bid-ask (buy-sell) spread, low commission, and high
+liquidity. 
+
+- eBay postiives: high liquidity with huge worldwide market.
+- eBay negatives: high overhead, high commissions, high fees, having to deal directly with other party (buyer or seller)
+
+- CL positives: no fees, no commissions.
+- CL negatives: smaller local market, low-ball offers, having to deal directly with customers, 
+    potential security risks (robbery, theft) 
+
+- LCS positives: local, usually quick cash-metals conversion
+- LCS negatives: high overhead, high commissions, high bid-ask (buy-sell) spreads, having to deal with low 
+
+When you are dealing directly with the other party (buyer/seller) and dealing with non-traceable assets 
+(cash and precious metals), you are potentially putting yourself in a high-risk situation for theft. 
+If you conduct the transaction at your house or via mail, your buyer or seller knows your address and that you 
+have high value items (precious metals, cash, etc.) located there. 
+If you conduct business elsewhere, they still will 
+know your face and/or vehicle and could potentially 
+follow you home. If it is not a secure place, they could still rob you. 
