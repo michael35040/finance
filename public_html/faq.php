815a8963da158c40834fe256b63d29645d161ed8
michael35040 <michaelsjohnson@gmail.com>
2015-03-30 08:25:14 -0400

    Create faq.php

---
+++ public_html/faq.php
@@ -0,0 +1,6 @@
+<b>What is an 'orderbook'?</b>
+<br>
+An electronic list of buy and sell orders for a specific security or financial instrument, organized 
+by price level. The order book lists the number of shares being bid or offered at each price point, 
+or market depth. It also identifies the market participants behind the buy and sell orders. The order 
+book is dynamic and constantly updated in real time throughout the day. 
