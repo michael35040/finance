df57b8bbc10342ccba8f358cca87bc0c9c8ef0b7
michael35040 <michaelsjohnson@gmail.com>
2015-05-11 08:54:49 -0400

    Update guess.php

--- public_html/spot/guess.php
+++ public_html/spot/guess.php
@@ -6,8 +6,65 @@
 
 //PULL NY SPOT
 
-//SHOW AVAIALBE GUESSES
+
 //PULL DB QUERY OF CURRENT GUESSES
+$event = 1;
+$guesses =	query("SELECT id, price, name, date FROM spot WHERE event = ? ORDER BY price ASC", $event);
+
+
+//SHOW AVAIALBE GUESSES
+if(!empty($guesses)) 
+  {?>
+    <table class="table table-striped table-condensed table-bordered" >
+    <tr class="danger">
+      <td colspan="3" style="font-size:20px; text-align: center;">SPOT GUESS
+      </td>
+    </tr>
+    <tr>
+      <td>GUESS</td>
+      <td>PRICE</td>
+      <td>USER</td>
+      <td>DATE</td>
+    </tr>
+  <?php foreach ($guesses as $guess) 
+  { ?>
+    <tr>
+      <td>
+        <form method="post" action="guess.php">
+          <button type="submit" class="btn btn-danger btn-xs" name="guess" value="<?php echo($spotprice) ?>">
+            <span class="glyphicon glyphicon-remove-circle"></span>
+          </button>
+        </form>
+      </td>
+
+      <td>
+        <?php echo(htmlspecialchars($guess["price"] )); ?>
+      </td>
+      <td>
+        <?php echo(htmlspecialchars( $guess["id"] )); ?>
+      </td>
+      <td>
+        <?php echo(htmlspecialchars(date('F j, Y, g:ia', strtotime($guess["date"])) )); ?>
+      </td>    
+      
+      
+    </tr>
+  <?php
+  } //if foreach
+  ?>
+
+
+</table>
+<?php 
+} //!empty 
+else
+{
+  echo "empty!";
+}
+
+
+
+
 //SPIT OUT ALL NUMBERS WITH BOXES
 //THOSE AVAILABLE CAN CLICK FOR GUESS
 //THOSE TAKEN DISPLAY THOSE WHO GUESS
