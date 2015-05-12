726420a62075a64de5e87fa72ac4f7664c2e552b
michael35040 <michaelsjohnson@gmail.com>
2015-05-12 17:16:09 -0400

    Create guess.php

---
+++ public_html/guess.php
@@ -0,0 +1,19 @@
+<?php
+require("../includes/config.php");
+// if form was submitted
+$title = "Guess";
+$id =  $_SESSION["id"];
+
+//Guess
+//$history = query("SELECT * FROM history WHERE (id = ? AND (TRANSACTION='TRANSFER' OR TRANSACTION='DEPOSIT' OR TRANSACTION='WITHDRAW')) ORDER BY uid DESC $limit", $id);
+//$error = query("SELECT * FROM error WHERE (id = ?) ORDER BY uid DESC $limit", $id);
+
+render(
+    "guess_form.php",
+    [
+        "title" => $title,
+        //"history" => $history,
+        //"error" => $error,
+    ]);
+
+?>
