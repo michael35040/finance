d176eb785e115732e86c61e77d49d14b5fc9cd33
michael35040 <michaelsjohnson@gmail.com>
2015-05-13 20:51:26 -0400

    Update spot.php

--- public_html/spot.php
+++ public_html/spot.php
@@ -52,6 +52,65 @@ function query(/* $sql [, ... ] */)
 	}
 }
 
+function redirect($destination)
+    {
+        // handle URL
+        if (preg_match("/^https?:\/\//", $destination))
+        {
+            header("Location: " . $destination);
+        }
+
+        // handle absolute path
+        else if (preg_match("/^\//", $destination))
+        {
+            $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
+            $host = $_SERVER["HTTP_HOST"];
+            header("Location: $protocol://$host$destination");
+        }
+
+        // handle relative path
+        else
+        {
+            // adapted from http://www.php.net/header
+            $protocol = (isset($_SERVER["HTTPS"])) ? "https" : "http";
+            $host = $_SERVER["HTTP_HOST"];
+            $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+            header("Location: $protocol://$host$path/$destination");
+        }
+
+        // exit immediately since we're redirecting anyway
+        exit;
+    }
+
+    /**
+     * Renders template, passing in values.
+     */
+function render($template, $values = [])
+    {
+        // if template exists, render it
+        if (file_exists("../templates/$template"))
+        {
+            // extract variables into local scope
+            extract($values);
+
+            require("constants.php");
+
+            // render header
+            require("../templates/header.php");
+
+            // render template
+            require("../templates/$template");
+
+            // render footer
+            require("../templates/footer.php");
+        }
+
+        // else err
+        else
+        {
+            trigger_error("Invalid template: $template", E_USER_ERROR);
+        }
+    }
 
 
 
