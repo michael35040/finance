0c775ea20bc984af4713628f6fec5d63e3b7b39c
michael35040 <michaelsjohnson@gmail.com>
2015-05-13 22:16:44 -0400

    Update guess.php

--- public_html/guess.php
+++ public_html/guess.php
@@ -1,8 +1,6 @@
 <?php
 require("../includes/config.php");
 // if form was submitted
-$title = "Guess";
-if(!isset($_SESSION["id"])){$id=0;}else{$id=$_SESSION["id"];}
 
 //Guess
 //$history = query("SELECT * FROM history WHERE (id = ? AND (TRANSACTION='TRANSFER' OR TRANSACTION='DEPOSIT' OR TRANSACTION='WITHDRAW')) ORDER BY uid DESC $limit", $id);
@@ -11,7 +9,7 @@ if(!isset($_SESSION["id"])){$id=0;}else{$id=$_SESSION["id"];}
 render(
     "guess_form.php",
     [
-        "title" => $title,
+        //"title" => $title,
         //"history" => $history,
         //"error" => $error,
     ]);
