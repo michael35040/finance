6238de95ec9fc11957c867841ee3fc999fc1b421
michael35040 <michaelsjohnson@gmail.com>
2015-05-14 21:24:27 -0400

    Update spot.php

--- public_html/spot.php
+++ public_html/spot.php
@@ -732,7 +732,7 @@ foreach ($guesses as $guess)
 </div> <!--middle -->
 <div id="bottom"  style="text-shadow: 1px 1px 5px #000; z-index:-1;">
 <br>
- <?php echo(htmlspecialchars($sitename))?> &#169; 1985-2014
+ <?php echo(htmlspecialchars($sitename))?> &#169; 1776-2020
 
 <h6 style='text-align:center; text-shadow: 1px 1px 2px #000; color:white;'>
 Server Time: <?php echo(date('Y-m-d H:i:s')); ?>
