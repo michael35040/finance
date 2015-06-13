a1e49d1dfaf494d7a841f2915f21c8514bea0a48
michael35040 <michaelsjohnson@gmail.com>
2015-06-12 21:39:54 -0400

    Update sizer.php

--- public_html/sizer.php
+++ public_html/sizer.php
@@ -1,42 +1,62 @@
-//SEE IF USER NEEDS TO MAKE A GUESS
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
 {
+    if (isset($_POST["event"])){$event=$_POST["event"];}
+    $currentstackvalue=$_POST["currentstackvalue"];
+    $movement=$_POST["movement"];
+    $newpurchaseozt=$_POST["newpurchaseozt"];
+    $newpurchasepriceozt=$_POST["newpurchasepriceozt"];
+}
 
-
-    if (isset($_POST["event"]))
-    {
-        $event=$_POST["event"];
-        if($event=='All')
-        {
-            $guesses =	query("SELECT uid, id, price, name, date, dateend, event FROM spot ORDER BY price ASC, event ASC");
-
+//Purchase Price Total
+$newpurchaseprice=$newpurchasepriceozt*$newpurchaseozt;
         
-
-$currentstackprice
-$movement
-$newpurchaseozt
-$newpurchaseprice=_POST...
-
-
-$currentstackozt=null;
-
-
 $currentstacksize=0;
 while ($movement != $calcmovement)
 {
-
-
-$currentstackvalue=$currentstacksize*$currentstackprice;
-
-$newstacksize=$currentstacksize+$newpurchaseozt;
-
-$newstackvalue=($currentstackvalue+$newpurchaseprice)/$newstacksize;
-
-
-$currentstacksize++;
-
-if($movement==$calcmovement){
-echo($currentstacksize);
-break;}
+    //Calculate the Value of the Current Stack
+    $currentstackvalue=$currentstacksize*$currentstackprice;
+    
+    //Calculate the Size of the New Stack from Old Stack and New Purchase
+    $newstacksize=$currentstacksize+$newpurchaseozt;
+    
+    //Calculate the New Value of the Stack
+    $newstackvalue=($currentstackvalue+$newpurchaseprice)/$newstacksize;
+    
+    //Calculate Movement
+    $calcmovement=$newstackvalue-$currentstackvalue;
+    
+    
+    //If Movement equals New Movement, Stop While Loop
+    if($movement==$calcmovement)
+    {
+        echo($currentstacksize);
+        break;
+    }
+    
+    //Increase the Stack Size
+    $currentstacksize++;
+        
 }
+?>
+ <form method="post" action="sizer.php">
+                            
+  
+  Current Stack Price per Troy Ounce:
+  <input type="number" name="currentstackvalue" min="0.01" step="0.01" max="100">
+  <br>
+  
+  Movement if New Purchase:
+  <input type="number" name="movement" min="0.01" step="0.01" max="100">
+  <br>
+  
+  New Purchase Troy Ounce:
+  <input type="number" name="newpurchaseozt" min="0.01" step="0.01" max="10000">
+  <br>
+  
+  New Purchase Price Per Ounce
+  <input type="number" name="newpurchasepriceozt" min="0.01" step="0.01" max="10000">
+  <br>
 
+  <button type="submit" >Submit</button>
+ </form>
