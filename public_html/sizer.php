a11e8446d1c721aec2897683fec2903fbf6e7b76
michael35040 <michaelsjohnson@gmail.com>
2015-06-15 21:16:26 -0400

    Update sizer.php

--- public_html/sizer.php
+++ public_html/sizer.php
@@ -1,159 +1,47 @@
 <?php
 echo('<font color="red">');
-
 if ($_SERVER["REQUEST_METHOD"] == "POST")// if form is submitted
 {
     if (isset($_POST["event"])){$event=$_POST["event"];}
-    $currentstackpriceperozt=$_POST["currentstackvalue"];
-    $newpurchaseozt=$_POST["newpurchaseozt"];
-    $newpurchasepriceozt=$_POST["newpurchasepriceozt"];
-    $movement=$_POST["movement"];
-
-    /*
-    $a=$currentstackpriceperozt;
-    $b=$newpurchaseozt;
-    $c=$newpurchasepriceozt;
-    $d=$movement;
-    $d=(((($e*$b)*$a)+($b*$c))/($e*$b))-$a;
-    d=((((e*b)*a)+(b*c))/(e*b))-a;
-    
-    //ANOTHER SOLVE
-    d=((ae+c)/e)-a
-    
-    
-    //POSSIBLE SOLVES BY ALGEBRA CALCULATOR
-    $d=((2.718282ba+bc)/2.718282b)-a;
-    $d=$c/$e;
-    $c=(2.718282*$d);
-    $d=(0.367879*$c);
-    //$e='current stack size';
-    */
-    
-    
-//CHECK TO MAKE SURE THE MOVEMENT IS CURRENT VALUE
-//IF NEGATIVE MOVEMENT, NEW PURCHASE PRICE MUST BE LOWER
-if($movement<0 && $newpurchasepriceozt>$currentstackpriceperozt):
-{
-    echo("Invalid numbers.<br>
-        'Movement' (D) must be positive if the value of the 'New Purchaes Price' (C) is higher than 'Current Price of Stack' (A).");
-}
-elseif($movement>0 && $newpurchasepriceozt<$currentstackpriceperozt):
-{
-    echo("Invalid numbers.<br>
-        'Movement' (D) must be negative (-) if the value of the 'New Purchaes Price' (C) is lower than 'Current Price of Stack' (A).");
-}
-elseif($currentstackpriceperozt>100):
-{
-    echo("Please check the 'Current Stack Price of Stack' (A) value is the price per ounce, not the total value.");
-}
-elseif($newpurchaseozt>100):
-{
-    echo("Please check the 'New Purchase Pirce' (C) value is the price per ounce, not the total value.");
-}
-elseif($movement>20):
-{
-    echo("Please check the 'Movement' (D) value is the price per ounce, not the total value.");
-}
-else:
-{
-    echo("
-        Current Value: " . $currentstackpriceperozt . " | 
-        Movement: " . $movement . " | 
-        New Purchase: " . $newpurchaseozt . " ozt |
-        New Purchase: $" . $newpurchasepriceozt
-    );
-
-    //Purchase Price Total
-    $newpurchaseprice=$newpurchasepriceozt*$newpurchaseozt;
-            
-    $currentstacksize=0;
-    $calcmovement=-100;
-    
-   
-    
-    //CREATE ARRAY FOR THE POSSIBLE CURRENT STACK SIZE
-    $stackArray = array();
-    
-
-    //while ($movement != $calcmovement)
-    while ($movement != $calcmovement) 
-    {
-        //Calculate the Value of the Current Stack
-            $currentstackvalue=$currentstacksize*$currentstackpriceperozt;
-            $currentstackvalue=round($currentstackvalue, 2);
-        //Calculate the Size of the New Stack from Old Stack and New Purchase
-            $newstacksize=$currentstacksize+$newpurchaseozt;
-        //Calculate the New Value of the Stack
-            $newstackvalue=($currentstackvalue+$newpurchaseprice);
-            $newstackvalue=round($newstackvalue, 2);
-            $newstackvalueozt=($currentstackvalue+$newpurchaseprice)/$newstacksize;
-            $newstackvalueozt=round($newstackvalueozt, 2);
-        //Calculate Movement
-            $calcmovement=$newstackvalueozt-$currentstackpriceperozt;
-            $calcmovement=round($calcmovement, 2);
-        //If Movement equals New Movement, Stop While Loop
-            
-            //POSSIBLE VALUE, ADD TO ARRAY
-            if($movement==$calcmovement){array_push($stackArray, $newstacksize);}
-            
-            //IF MOVEMENT IS NEGATIVE
-            if($movement<0){if($movement<$calcmovement){break;}}
-            //IF MOVEMENT IS POSITIVE
-            if($movement>0){if($movement>$calcmovement){break;}}
-            
-            
-            if($i==10000){$rangeStart=10000; $rangeEnd='higher'; break;}
-        
-        //Increase the Stack Size
-            $currentstacksize++;
-    }
-    
-    //print_r($stackArray);
-    $count=count($stackArray);
-    $end=$count-1;
-    
-    $rangeStart=$stackArray[0];
-    $rangeEnd=$stackArray[$end];
-    
-    echo("<br><br><b>Estimated Stack Size: " . $rangeStart . "-" . $rangeEnd . " troy ounces.</b><br><br>");
-    
-} //else
-endif;
+    $a=$_POST["a"];
+    $b=$_POST["b"];
+    $c=$_POST["c"];
+    $d=$_POST["d"];
+    $d=round($d, 5);
+    //CHECKS
+    if($d<0 && $c>$a){ echo("Invalid numbers.<br>'Movement' (D) must be positive if the value of the 'New Purchaes Price' (C) is higher than 'Current Price of Stack' (A).");}
+    if($d>0 && $c<$a){ echo("Invalid numbers.<br>'Movement' (D) must be negative (-) if the value of the 'New Purchaes Price' (C) is lower than 'Current Price of Stack' (A).");}
+    //SOLVE FOR X    
+    $x=((-$a*$b)+($b*$c)-($b*$d))/$d;
+    echo("Estimated size of stack: " . $x . "<hr>Current Value: " . $a . " | Movement: " . $d . " | New Purchase: " . $b . " ozt | New Purchase: $" . $c );
 } //if post
-
 echo('</font>');
-
 ?>
  <form method="post" action="sizer.php">
-                            
-  
-  <br>
   <h1>Reverse Compute Size of Current Stack</h1>
-  <br>
   <b>A.) Current Price of Stack ($/per ounce):</b>
-  <br>$<input type="number" name="currentstackvalue" min="0.01" step="0.01" max="100">/ozt 
+  <br>$<input type="number" name="a" min="0.01" step="0.01" max="100">/ozt 
   <br>(ex: 20.00)
   <br>
   Price per troy ounce of the stack. If you have 100 ozt and you spent $2,000, then your price per ounce is $20 ($2,000/100ozt).
   <hr>
-  
-  
+
   <b>B.) New Purchase Size (ounces):</b>
-  <br><input type="number" name="newpurchaseozt" min="0.01" step="0.01" max="10000"> ozt 
+  <br><input type="number" name="b" min="0.01" step="0.01" max="10000"> ozt 
   <br>(ex: 20)
   <br>
   The size of the purchase. If you are buying a roll of American Silver Eagles, then it would be 20.
   <hr>
   
   <b>C.) New Purchase Price ($/per ounce):</b>
-  <br><input type="number" name="newpurchasepriceozt" min="0.01" step="0.01" max="10000">/ozt 
+  <br><input type="number" name="c" min="0.01" step="0.01" max="10000">/ozt 
   <br>(ex: 22.00)
   <br>
   The price per ounce of the new purchase. If you are buying a roll of Eagles for $440, then it would be $22 ($440/20ozt).
   <hr>
 
   <b>D.) Movement ($/per ounce):</b>
-  <br>$<input type="number" name="movement" min="-100" step="0.01" max="100"> 
+  <br>$<input type="number" name="d" min="-100" step="0.00001" max="100"> 
   <br>(ex: 0.33)
   <br>
   *Ensure you use the negative (-) sign if it decreases the cost. No need to use the plus (+) sign for positive numbers.<br>
