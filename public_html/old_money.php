5611503c741337519a7a40bfeb86c6e918906f8a
michael35040 <michaelsjohnson@gmail.com>
2015-07-27 15:21:36 -0400

    Create old_money.php

---
+++ public_html/old_money.php
@@ -0,0 +1,123 @@
+
+http://www.slate.com/blogs/moneybox/2012/05/01/diocletian_and_inflation.html
+
+http://www.constantinethegreatcoins.com/edict/
+Argenteus (silver) was 3 grams (1/96th Roman Pound):
+293-300AD : 25dc
+300-301AD : 50dc
+301-307AD : 100dc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+How many U.S. 'Constitutional' Dollars you would have:
+
+
+
+
+
+
+
+
+
+Denarius:
+Predecessor 267BC: 6.81grams (1/48th Roman Pound)
+211BC: 4.5grams (1/72 Roman Pound)
+Augustus 63BC-14AD: 3.9grams (1/84 Roman Pound)
+Nero 37-68AD: 3.4grams (1/96 Roman Pound)
+3rd Century: 3 grams
+
+How many Roman Soldiers you could buy:
+Legionary (Professional Soldier) 
+===509-27BC: 112.5d/yr
+===Under Julius Ceaser: 225d/yr
+Centurions  (under Augustus 63BC-14AD)
+==Lowest Paid: 3,750d/yr
+==Highest Paid: 15,000d/yr
+
+How many day's you could pay a common laborer (Matthew 20:2, John 12:5):
+
+How many times you could pay off Judas to betray Jesus (Matthew 26:15) (~35AD):
+Judas used 30 'arguria', which simply means silver coins. 
+420 grams //30*Tyrian Shekel (14grams):
+516 grams //30*Athenian Tetradrachm(17.2=4.3grams*4) //1 drachma equals days of skilled labor, 1 tetradrachm equals 4 drachma 
+//30 pieces of silver equals slave (Exodus 21:32, Zechariah 11:13)
+
+
+
+How many liters of wheat you could buy (a choinix, equal to 1.09l, of wheat cost 1 denarius) biblical new testament times:
+
+How many liters of barley you could buy (3 qts of barley equal 1 denarius) biblical new testament times:
+
+
+
+
+
+
+http://www.constantinethegreatcoins.com/edict/
+http://ancientcoinsforeducation.org/content/view/79/98/
+https://books.google.com/books?id=KIFJiOCSYc8C&pg=PA44&lpg=PA44&dq=indenariicommunes&source=bl&ots=n720apv0qB&sig=P0__aLyw4OdlqYIQPZ8CWyzozKA&hl=en&sa=X&ved=0CC0Q6AEwA2oVChMI0qjPt_77xgIVyj0-Ch1xxQcv#v=onepage&q=indenariicommunes&f=false
+
+What you could buy in Diocletian (Rome) during the 3rd Century
+Farm Laborer monthly pay, with meals: 400 asses
+Teacher's monthly pay, per boy: 800 asses
+Barber's service price, per client: 32 asses
+1kg of pork = 380 asses (1lb=170asses)
+1kg of grapes = 32 asses (1lb=15asses)
+1 denarius = 16 asses (however, pre 141BC, 1 denarius 'contained ten' asses).
+1 'libra' (Roman Pound, 326 grams) of fine silk cost more than 12 humans
+
+
+
+What you could buy in Anglo-Saxon during the 10th Century (~950AD):
+<?php
+//The History of British Commerce: From the Earlist Times, Volume 1 (pg 85)
+$shilling= //1/20 pound or 12 pence
+$shilling=112.5; //grains when it contained 5 penny (1/48 pound)
+$penny=$shilling/5;
+$pound=$penny*240; //5400 grains (tower pound)
+//in grains
+$Man_or_slave=$pound;
+$horse=$shilling*30;
+$mare_or_colt=$shilling*20;
+$ass_or_mule=$shilling*12;
+$ox=$shilling*6;
+$cow=$shilling*5;
+$swine=($shilling*1)+($penny*3);
+$sheep=$shilling*1;
+$goat=$penny*2;
+?>
+
+
+
+'Monetary Metals' (Group 11 on Periodic Table)
+Minable World Resources
+Gold: 61,000t
+Silver: 390,000t
+Copper 570,000,000 tons
+
+gold: 2,490t
+silver: 19,690t
+copper: 14,500,000t
+
+
+
+
+
